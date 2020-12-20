4ce1cf2504ae1bcc33d35c333a37431ca76284d3
Sparkmod <rafadagher@gmail.com>
2020-12-19 23:29:13 -0400

    Create Achieve.cs

---
+++ MapleServer2/Types/Achieve.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace MapleServer2.Types {
+    public class Achieve {
+        public readonly int Id;
+        public byte Type;
+        public int StartGrade;
+        public int CurrentGrade;
+        public int EndGrade;
+        public long Count;
+        // Grade -> Timestamp of completed achievements
+        public IDictionary<int, long> Record;
+
+        public Achieve(int id) {
+            Id = id;
+            Record = new Dictionary<int, long>();
+        }
+    }
+}
