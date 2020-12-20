3557d42b61298dae3fc6b88aef6bbc5ee0cd8a40
Andy Vandenbroeck <13941478+Andoriii@users.noreply.github.com>
2020-12-21 00:04:38 +0100

    Format MapEntityStorage.cs

--- MapleServer2/Data/Static/MapEntityStorage.cs
+++ MapleServer2/Data/Static/MapEntityStorage.cs
@@ -4,14 +4,16 @@ using System.IO;
 using Maple2Storage.Types;
 using ProtoBuf;
 
-namespace MapleServer2.Data.Static {
+namespace MapleServer2.Data.Static
+{
     public static class MapEntityStorage
     {
         private static readonly Dictionary<int, List<MapNpc>> npcs = new Dictionary<int, List<MapNpc>>();
         private static readonly Dictionary<int, List<MapPortal>> portals = new Dictionary<int, List<MapPortal>>();
         private static readonly Dictionary<int, List<MapPlayerSpawn>> playerSpawns = new Dictionary<int, List<MapPlayerSpawn>>();
 
-        static MapEntityStorage() {
+        static MapEntityStorage()
+        {
             using FileStream stream = File.OpenRead("Maple2Storage/Resources/ms2-map-entity-metadata");
             List<MapEntityMetadata> entities = Serializer.Deserialize<List<MapEntityMetadata>>(stream);
             foreach (MapEntityMetadata entity in entities)
@@ -22,19 +24,23 @@ namespace MapleServer2.Data.Static {
             }
         }
 
-        public static IEnumerable<MapNpc> GetNpcs(int mapId) {
+        public static IEnumerable<MapNpc> GetNpcs(int mapId)
+        {
             return npcs.GetValueOrDefault(mapId);
         }
 
-        public static IEnumerable<MapPortal> GetPortals(int mapId) {
+        public static IEnumerable<MapPortal> GetPortals(int mapId)
+        {
             return portals.GetValueOrDefault(mapId);
         }
-        
-        public static IEnumerable<MapPlayerSpawn> GetPlayerSpawns(int mapId) {
+
+        public static IEnumerable<MapPlayerSpawn> GetPlayerSpawns(int mapId)
+        {
             return playerSpawns.GetValueOrDefault(mapId);
         }
 
-        public static MapPlayerSpawn GetRandomPlayerSpawn(int mapId) {
+        public static MapPlayerSpawn GetRandomPlayerSpawn(int mapId)
+        {
             List<MapPlayerSpawn> list = playerSpawns.GetValueOrDefault(mapId);
             return list?.Count > 0 ? list[new Random().Next(list.Count)] : null;
         }
