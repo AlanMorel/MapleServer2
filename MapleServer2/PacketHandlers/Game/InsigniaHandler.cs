b737021fbd317a33852e18051c4a5bf2e725e693
Zin <62830952+Zintixx@users.noreply.github.com>
2020-12-19 19:16:13 -0800

    Fixed for latest repo and updated formatting

--- MapleServer2/PacketHandlers/Game/InsigniaHandler.cs
+++ MapleServer2/PacketHandlers/Game/InsigniaHandler.cs
@@ -4,15 +4,17 @@ using MapleServer2.Packets;
 using MapleServer2.Servers.Game;
 using Microsoft.Extensions.Logging;
 
-namespace MapleServer2.PacketHandlers.Game {
-    public class InsigniaHandler : GamePacketHandler {
-        public override ushort OpCode => RecvOp.NAMETAG_SYMBOL;
+namespace MapleServer2.PacketHandlers.Game
+{
+    public class InsigniaHandler : GamePacketHandler
+    {
+        public override RecvOp OpCode => RecvOp.NAMETAG_SYMBOL;
 
         public InsigniaHandler(ILogger<InsigniaHandler> logger) : base(logger) { }
 
-        public override void Handle(GameSession session, PacketReader packet) {
+        public override void Handle(GameSession session, PacketReader packet)
+        {
             short insigniaID = packet.ReadShort();
-            System.Console.WriteLine("insigniaID: " + insigniaID);
             session.FieldManager.BroadcastPacket(InsigniaPacket.UpdateInsignia(session, insigniaID));
         }
     }
--- MapleServer2/Packets/InsigniaPacket.cs
+++ MapleServer2/Packets/InsigniaPacket.cs
@@ -3,13 +3,16 @@ using MapleServer2.Constants;
 using MapleServer2.Types;
 using MapleServer2.Servers.Game;
 
-namespace MapleServer2.Packets {
-    public static class InsigniaPacket {
-        public static Packet UpdateInsignia(GameSession session, short insigniaID) {
+namespace MapleServer2.Packets
+{
+    public static class InsigniaPacket
+    {
+        public static Packet UpdateInsignia(GameSession session, short insigniaID)
+        {
             PacketWriter pWriter = PacketWriter.Of(SendOp.NAME_TAG_SYMBOL);
-                pWriter.WriteInt(session.FieldPlayer.ObjectId);
-                pWriter.WriteShort(insigniaID);
-                pWriter.WriteByte(01); //01 = Display. 00 = Do not display.
+            pWriter.WriteInt(session.FieldPlayer.ObjectId);
+            pWriter.WriteShort(insigniaID);
+            pWriter.WriteByte(01); //01 = Display. 00 = Do not display.
             return pWriter;
         }
     }
