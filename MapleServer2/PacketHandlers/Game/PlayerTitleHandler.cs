dcf1d89d49c5f494184cefd4af09d3616f8de503
Zin <62830952+Zintixx@users.noreply.github.com>
2020-12-19 23:05:53 -0800

    Player Titles

---
+++ MapleServer2/PacketHandlers/Game/PlayerTitleHandler.cs
@@ -0,0 +1,22 @@
+﻿using MaplePacketLib2.Tools;
+using MapleServer2.Constants;
+using MapleServer2.Packets;
+using MapleServer2.Servers.Game;
+using Microsoft.Extensions.Logging;
+
+namespace MapleServer2.PacketHandlers.Game
+{
+    public class PlayerTitleHandler : GamePacketHandler
+    {
+        public override RecvOp OpCode => RecvOp.REQUEST_USER_ENV;
+
+        public PlayerTitleHandler(ILogger<PlayerTitleHandler> logger) : base(logger) { }
+
+        public override void Handle(GameSession session, PacketReader packet)
+        {
+            byte function = packet.ReadByte();
+            int playerTitleID = packet.ReadInt();
+            session.FieldManager.BroadcastPacket(PlayerTitlePacket.UpdatePlayerTitle(session, playerTitleID));
+        }
+    }
+}
