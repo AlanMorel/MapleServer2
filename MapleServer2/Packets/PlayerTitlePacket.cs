3a236928419eedbf2125d588bb05a60d8a929c17
Alan Morel <alan@alanmorel.com>
2020-12-22 14:59:59 -0500

    Improving title and insignia integration

--- MapleServer2/Packets/PlayerTitlePacket.cs
+++ MapleServer2/Packets/PlayerTitlePacket.cs
@@ -1,18 +1,17 @@
 ﻿using MaplePacketLib2.Tools;
 using MapleServer2.Constants;
-using MapleServer2.Types;
 using MapleServer2.Servers.Game;
 
 namespace MapleServer2.Packets
 {
     public static class PlayerTitlePacket
     {
-        public static Packet UpdatePlayerTitle(GameSession session, int playerTitleID)
+        public static Packet UpdatePlayerTitle(GameSession session, int titleId)
         {
             PacketWriter pWriter = PacketWriter.Of(SendOp.USER_ENV);
             pWriter.WriteByte(01); // Mode update
             pWriter.WriteInt(session.FieldPlayer.ObjectId);
-            pWriter.WriteInt(playerTitleID);
+            pWriter.WriteInt(titleId);
             return pWriter;
         }
     }
