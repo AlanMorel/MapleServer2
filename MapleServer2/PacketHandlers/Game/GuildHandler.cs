052e0096431befb19f5f04f2fb329f2acfd38552
Zin <62830952+Zintixx@users.noreply.github.com>
2021-01-03 19:46:15 -0800

    Guild creation

---
+++ MapleServer2/PacketHandlers/Game/GuildHandler.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;
+using MaplePacketLib2.Tools;
+using MapleServer2.Constants;
+using MapleServer2.Packets;
+using MapleServer2.Servers.Game;
+using Microsoft.Extensions.Logging;
+using MapleServer2.Types;
+
+namespace MapleServer2.PacketHandlers.Game
+{
+    public class GuildHandler : GamePacketHandler
+    {
+        public override RecvOp OpCode => RecvOp.GUILD;
+
+        public GuildHandler(ILogger<GuildHandler> logger) : base(logger) { }
+
+        private enum GuildMode : byte
+        {
+            Create = 0x1,
+            CheckIn = 0xF,
+            EnterHouse = 0x64,
+            GuildWindow = 0x54,
+            List = 0x55,
+            GuildNotice = 0x3E,
+            GuildDonate = 0x6E,
+        }
+
+        public override void Handle(GameSession session, PacketReader packet)
+        {
+            GuildMode mode = (GuildMode) packet.ReadByte();
+            switch (mode)
+            {
+                case GuildMode.Create:
+                    HandleCreate(session, packet);
+                    break;
+                case GuildMode.CheckIn:
+                    HandleCheckIn(session, packet);
+                    break;
+                case GuildMode.EnterHouse:
+                    HandleEnterHouse(session, packet);
+                    break;
+                case GuildMode.GuildWindow:
+                    HandleGuildWindowRequest(session, packet);
+                    break;
+                case GuildMode.List:
+                    HandleList(session, packet);
+                    break;
+                case GuildMode.GuildNotice:
+                    HandleGuildNotice(session, packet);
+                    break;
+                case GuildMode.GuildDonate:
+                    HandleGuildDonate(session, packet);
+                    break;
+                default:
+                    IPacketHandler<GameSession>.LogUnknownMode(mode);
+                    break;
+            }
+        }
+
+        private void HandleCreate(GameSession session, PacketReader packet)
+        {
+            string guildName = packet.ReadUnicodeString();
+
+            session.Player.Mesos -= 2000;
+
+            session.Send(MesosPacket.UpdateMesos(session));
+            session.Send(GuildPacket.Invite(session.Player, guildName));
+            session.Send(GuildPacket.Create(guildName));
+            session.Send(GuildPacket.UpdateGuild(session, guildName));
+            session.Send(GuildPacket.Create2(session.Player, guildName));
+            session.Send(GuildPacket.Create3(session.Player, guildName));
+
+            Guild newGuild = new(guildName, new List<Player> { session.Player });
+            GameServer.GuildManager.AddGuild(newGuild);
+
+            newGuild.AddMember(session.Player);
+        }
+
+        private void HandleCheckIn(GameSession session, PacketReader packet)
+        {
+            session.Send(GuildPacket.CheckInConfirm());
+            // TODO: Send Guild Coins
+            // TODO: Can only check in once a day
+            session.Send(GuildPacket.UpdateGuildFunds());
+            session.Send(GuildPacket.UpdateGuildExp());
+            session.Send(GuildPacket.UpdateGuildFunds2());
+            session.Send(GuildPacket.UpdatePlayerContribution(session.Player));
+            session.Send(GuildPacket.UpdatePlayerContribution(session.Player));
+        }
+
+        private void HandleGuildWindowRequest(GameSession session, PacketReader packet)
+        {
+            session.Send(GuildPacket.GuildWindow());
+        }
+
+        private void HandleEnterHouse(GameSession session, PacketReader packet)
+        {
+            // TODO
+        }
+
+        private void HandleList(GameSession session, PacketReader packet)
+        {
+            session.Send(GuildPacket.List());
+        }
+
+        private void HandleGuildNotice(GameSession session, PacketReader packet)
+        {
+            packet.ReadByte();
+            string notice = packet.ReadUnicodeString();
+
+            session.Send(GuildPacket.GuildNoticeConfirm(notice));
+            session.Send(GuildPacket.GuildNoticeChange(session.Player, notice)); // TODO: Change to Broadcast to Guild
+        }
+
+        private void HandleGuildDonate(GameSession session, PacketReader packet)
+        {
+            int donateQuantity = packet.ReadInt();
+
+            session.Send(GuildPacket.UpdateGuildFunds());
+            session.Send(GuildPacket.UpdatePlayerDonation());
+            session.Send(GuildPacket.UpdateGuildFunds2());
+            session.Send(GuildPacket.UpdatePlayerContribution(session.Player));
+        }
+    }
+}
---
+++ MapleServer2/Packets/GuildPacket.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using MaplePacketLib2.Tools;
+using MapleServer2.Constants;
+using MapleServer2.Servers.Game;
+using MapleServer2.Types;
+
+namespace MapleServer2.Packets
+{
+    public static class GuildPacket
+    {
+        private enum GuildPacketMode : byte
+        {
+            UpdateGuild = 0x0,
+            Create = 0x1,
+            Invite = 0x4C,
+            CheckInConfirm = 0xF,
+            Create2 = 0x12,
+            GuildNoticeChange = 0x1A,
+            Create3 = 0x20,
+            Unk1 = 0x24,
+            UpdateGuildExp = 0x31,
+            UpdateGuildFunds = 0x32,
+            UpdatePlayerContribution = 0x33,
+            GuildNoticeConfirm = 0x3E,
+            GuildWindow = 0x54,
+            List = 0x5A,
+            UpdateGuildFunds2 = 0x59, //unk
+            UpdatePlayerDonation = 0x6E,
+        }
+
+        public static Packet Invite(Player player, string guildName)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.GUILD);
+            pWriter.WriteByte((byte) GuildPacketMode.Invite);
+            pWriter.WriteUnicodeString(player.Name);
+            pWriter.WriteUnicodeString(guildName);
+            return pWriter;
+        }
+
+        public static Packet Create(string guildName)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.GUILD);
+            pWriter.WriteByte((byte) GuildPacketMode.Create);
+            pWriter.WriteByte(0x0);
+            pWriter.WriteUnicodeString(guildName);
+            return pWriter;
+        }
+
+        public static Packet UpdateGuild(GameSession session, string guildName)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.GUILD);
+            pWriter.WriteByte(0x0);
+            pWriter.WriteLong(1); // guild ID
+            pWriter.WriteUnicodeString(guildName);
+            pWriter.WriteUnicodeString(""); // guildMark Url
+            pWriter.WriteByte(0x3C); // guildC.apacity
+            pWriter.WriteUnicodeString("");
+            pWriter.WriteUnicodeString(""); // guild.Notice
+            pWriter.WriteLong(session.Player.AccountId); // guild.Leader.AccountId
+            pWriter.WriteLong(session.Player.CharacterId); // guild.Leader.CharacterId
+            pWriter.WriteUnicodeString(session.Player.Name); // guild.Leader.Player.Name
+            pWriter.WriteLong(1609108139); // guild creation timestamp
+            pWriter.WriteByte(0x0);
+            pWriter.WriteInt(1000);
+            pWriter.WriteInt(0);
+            pWriter.WriteInt(0);
+            pWriter.WriteInt(0);
+            pWriter.WriteInt(0);
+            pWriter.WriteInt(0);
+            pWriter.WriteInt(0);
+            pWriter.WriteInt(0);
+            pWriter.WriteByte(0x1);
+            pWriter.WriteInt(0);
+            pWriter.WriteInt(0); // guild.Exp
+            pWriter.WriteInt(0); // guild.Funds
+            pWriter.WriteByte(0x0);
+            pWriter.WriteInt(0);
+            pWriter.WriteByte(0x0); // guild.Members.Count and also loop for each member ?
+            pWriter.WriteByte(0x6); // loop for guild ranks ?
+            pWriter.WriteByte(0x0);
+            pWriter.WriteUnicodeString("Leader");
+            pWriter.WriteInt(4095);
+            pWriter.WriteByte(0x1);
+            pWriter.WriteUnicodeString("Jr.Leader");
+            pWriter.WriteInt(1);
+            pWriter.WriteByte(0x2);
+            pWriter.WriteUnicodeString("Member");
+            pWriter.WriteInt(1);
+            pWriter.WriteByte(0x3);
+            pWriter.WriteUnicodeString("NewMember");
+            pWriter.WriteInt(1);
+            pWriter.WriteByte(0x4);
+            pWriter.WriteUnicodeString("Member2");
+            pWriter.WriteInt(1);
+            pWriter.WriteByte(0x5);
+            pWriter.WriteUnicodeString("NewMember2");
+            pWriter.WriteInt(1);
+            pWriter.WriteByte(0x9); // loop for all guild skills
+            pWriter.WriteInt(1); // guild skill ID
+            pWriter.WriteInt(1); // guild skill level
+            pWriter.WriteLong(0);
+            pWriter.WriteInt(2);
+            pWriter.WriteInt(1);
+            pWriter.WriteLong(0);
+            pWriter.WriteInt(3);
+            pWriter.WriteInt(1);
+            pWriter.WriteLong(0);
+            pWriter.WriteInt(4);
+            pWriter.WriteInt(1);
+            pWriter.WriteLong(0);
+            pWriter.WriteInt(10001);
+            pWriter.WriteInt(1);
+            pWriter.WriteLong(0);
+            pWriter.WriteInt(10002);
+            pWriter.WriteInt(1);
+            pWriter.WriteLong(0);
+            pWriter.WriteInt(10003);
+            pWriter.WriteInt(1);
+            pWriter.WriteLong(0);
+            pWriter.WriteInt(10004);
+            pWriter.WriteInt(1);
+            pWriter.WriteLong(0);
+            pWriter.WriteInt(10005);
+            pWriter.WriteInt(1);
+            pWriter.WriteLong(0);
+            pWriter.WriteByte(0x4); // another loop. unk
+            pWriter.WriteInt(1);
+            pWriter.WriteInt(0);
+            pWriter.WriteInt(2);
+            pWriter.WriteInt(0);
+            pWriter.WriteInt(3);
+            pWriter.WriteInt(0);
+            pWriter.WriteInt(4);
+            pWriter.WriteInt(0);
+            pWriter.WriteInt(1);
+            pWriter.WriteInt(1);
+            pWriter.WriteInt(0);
+            pWriter.WriteByte(0x0);
+            pWriter.WriteByte(0x1);
+            pWriter.WriteShort(0);
+            pWriter.WriteInt(0);
+            pWriter.WriteInt(0);
+            pWriter.WriteUnicodeString("");
+            pWriter.WriteLong(0);
+            pWriter.WriteLong(0);
+            pWriter.WriteInt(0);
+            pWriter.WriteInt(0);
+            pWriter.WriteInt(0);
+            pWriter.WriteInt(0);
+            pWriter.WriteInt(0);
+            pWriter.WriteInt(0);
+            pWriter.WriteInt(0);
+
+            return pWriter;
+        }
+
+        public static Packet CheckInConfirm()
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.GUILD);
+            pWriter.WriteByte((byte) GuildPacketMode.CheckInConfirm);
+            return pWriter;
+        }
+
+        public static Packet Create2(Player player, string guildName)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.GUILD);
+            pWriter.WriteByte((byte) GuildPacketMode.Create2);
+            pWriter.WriteShort();
+            pWriter.WriteUnicodeString(player.Name);
+            pWriter.WriteByte(); //unk
+            pWriter.WriteShort(0x3);
+            pWriter.WriteLong(0x24161B5EC67A0400); //unk guildId?
+            pWriter.WriteLong(player.AccountId);
+            pWriter.WriteLong(player.CharacterId);
+            pWriter.WriteUnicodeString(player.Name);
+            pWriter.WriteByte(); //unk
+            pWriter.WriteInt(player.JobGroupId);
+            pWriter.WriteInt(player.JobId);
+            pWriter.WriteShort(player.Level);
+            pWriter.WriteInt(); //unk
+            pWriter.WriteInt(player.MapId);
+            pWriter.WriteShort(); //player.Channel
+            pWriter.WriteUnicodeString(player.ProfileUrl);
+            pWriter.WriteInt(); //player home mapID
+            pWriter.WriteInt(); // unk
+            pWriter.WriteInt(); // unk
+            pWriter.WriteLong(); //player home mapID expiration time
+            pWriter.WriteInt(); //combat trophy count
+            pWriter.WriteInt(); //adventure trophy count
+            pWriter.WriteInt(); //lifestyle trophy count
+            pWriter.WriteShort(); //unk, filler?
+            pWriter.WriteLong(); //timestamp
+            pWriter.WriteLong(); //timestamp
+            pWriter.WriteLong(); //timestamp
+            pWriter.WriteLong(); //unk
+            pWriter.WriteLong(); //unk
+            pWriter.WriteLong(); //unk
+            pWriter.WriteLong(); //unk
+            pWriter.WriteByte(); //unk
+            return pWriter;
+        }
+
+        public static Packet GuildNoticeChange(Player player, string notice)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.GUILD);
+            pWriter.WriteByte((byte) GuildPacketMode.GuildNoticeChange);
+            pWriter.WriteUnicodeString(player.Name);
+            pWriter.WriteByte(0x1);
+            pWriter.WriteUnicodeString(notice);
+            return pWriter;
+        }
+
+        public static Packet Create3(Player player, string guildName)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.GUILD);
+            pWriter.WriteByte((byte) GuildPacketMode.Create3);
+            pWriter.WriteUnicodeString(player.Name);
+            pWriter.WriteLong(player.AccountId);
+            pWriter.WriteLong(player.CharacterId);
+            pWriter.WriteUnicodeString(player.Name);
+            pWriter.WriteByte(); //unk
+            pWriter.WriteInt(player.JobGroupId);
+            pWriter.WriteInt(player.JobId);
+            pWriter.WriteShort(player.Level);
+            pWriter.WriteInt(); //unk
+            pWriter.WriteInt(player.MapId);
+            pWriter.WriteShort(); // player.Channel
+            pWriter.WriteUnicodeString(player.ProfileUrl);
+            pWriter.WriteInt(); // player home mapID
+            pWriter.WriteInt(); // unk
+            pWriter.WriteInt(); // unk
+            pWriter.WriteLong(); // player home mapID expiration time
+            pWriter.WriteInt(); // combat trophy count
+            pWriter.WriteInt(); // adventure trophy count
+            pWriter.WriteInt(); // lifestyle trophy count
+            return pWriter;
+        }
+
+        public static Packet Unk1(Player player)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.GUILD);
+            pWriter.WriteByte((byte) GuildPacketMode.Unk1);
+            pWriter.WriteUnicodeString(player.Name);
+            pWriter.WriteInt();
+            pWriter.WriteInt();
+            return pWriter;
+        }
+
+        public static Packet UpdateGuildExp()
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.GUILD);
+            pWriter.WriteByte((byte) GuildPacketMode.UpdateGuildExp);
+            pWriter.WriteInt(50000); // guildExp
+            return pWriter;
+        }
+
+        public static Packet UpdateGuildFunds()
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.GUILD);
+            pWriter.WriteByte((byte) GuildPacketMode.UpdateGuildFunds);
+            pWriter.WriteInt(10000000); // guildFunds
+            return pWriter;
+        }
+
+        public static Packet UpdatePlayerContribution(Player player)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.GUILD);
+            pWriter.WriteByte((byte) GuildPacketMode.UpdatePlayerContribution);
+            pWriter.WriteUnicodeString(player.Name);
+            pWriter.WriteInt();
+            pWriter.WriteInt(20); // Contribution today
+            pWriter.WriteInt(100); // Total contribution
+            return pWriter;
+        }
+
+        public static Packet GuildNoticeConfirm(string notice)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.GUILD);
+            pWriter.WriteByte((byte) GuildPacketMode.GuildNoticeConfirm);
+            pWriter.WriteByte(0x1);
+            pWriter.WriteUnicodeString(notice);
+            return pWriter;
+        }
+
+        public static Packet GuildWindow()
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.GUILD);
+            pWriter.WriteByte((byte) GuildPacketMode.GuildWindow);
+            pWriter.WriteInt();
+            return pWriter;
+        }
+
+        public static Packet List()
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.GUILD);
+            pWriter.WriteInt(0x5A); //amount of guilds to display. Currently it sets at 90.
+            pWriter.WriteByte(0x1);
+            pWriter.WriteLong();
+            pWriter.WriteLong();
+            pWriter.WriteLong();
+            pWriter.WriteShort();
+            pWriter.WriteUnicodeString(""); //GuildMark url
+            pWriter.WriteInt();
+            pWriter.WriteInt();
+            pWriter.WriteInt(); //guild Member Total
+            pWriter.WriteInt(); //guild Capacity
+            return pWriter;
+        }
+
+        public static Packet UpdateGuildFunds2()
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.GUILD);
+            pWriter.WriteByte((byte) GuildPacketMode.UpdateGuildFunds2);
+            pWriter.WriteInt(80); //guildExp gain
+            pWriter.WriteInt(10000); // guildFunds gain
+            return pWriter;
+        }
+
+        public static Packet UpdatePlayerDonation()
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.GUILD);
+            pWriter.WriteByte((byte) GuildPacketMode.UpdatePlayerDonation);
+            pWriter.WriteInt(0x3); // total amount of donations today
+            pWriter.WriteLong(DateTimeOffset.Now.ToUnixTimeSeconds() + Environment.TickCount); // timestamp now
+            return pWriter;
+        }
+    }
+}
