b2cf4d675ef48d3ab1d883ab3e24b1242a149517
bwu113 <bowenwu112@gmail.com>
2021-01-04 19:17:04 -0500

    test

---
+++ DatabaseHandler/DatabaseController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseHandler
+{
+    public class DatabaseController
+    {
+        public Account GetAccountByUser(string username)
+        {           
+            using (Maplestory2DBEntities context = new Maplestory2DBEntities())
+            {
+                Account accountInfo = context.Accounts.FirstOrDefault(r => r.Username == username);
+                return accountInfo;
+            }
+        }
+
+        public void CreateChar(long AccID, string name, byte gender, int JobId, int MapId)
+        {
+            using (Maplestory2DBEntities context = new Maplestory2DBEntities())
+            {
+                Character character = new Character
+                {
+                    Account_ID = AccID,
+                    Name = name,
+                    Gender = gender,
+                    Level = (short) JobId,
+                    Job_ID = (short) JobId,
+                    Map_ID = MapId
+                };
+            }
+        }
+
+        public Character GetCharByID(long charId)
+        {
+            using (Maplestory2DBEntities context = new Maplestory2DBEntities())
+            {
+                Character charInfo = context.Characters.FirstOrDefault(r => r.Character_ID == charId);
+                return charInfo;
+            }
+        }
+    }
+}
--- MapleServer2/PacketHandlers/Login/LoginHandler.cs
+++ MapleServer2/PacketHandlers/Login/LoginHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DatabaseHandler;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Net;
@@ -34,22 +35,21 @@ namespace MapleServer2.PacketHandlers.Login
         public override void Handle(LoginSession session, PacketReader packet)
         {
             byte mode = packet.ReadByte();
-            string username = packet.ReadUnicodeString();
-            string pass = packet.ReadUnicodeString();
+            DatabaseController DB = new DatabaseController();
+            Account User = DB.GetAccountByUser(packet.ReadUnicodeString());
+
+            string username = User.Username;
+            string pass = User.Password;
 
             logger.Debug($"Logging in with username: '{username}' pass: '{pass}'");
 
             // TODO: From this user/pass lookup we should be able to find the accountId
-<<<<<<< HEAD
             if (string.IsNullOrEmpty(username) && string.IsNullOrEmpty(pass))
             {
-=======
-            if (string.IsNullOrEmpty(username) && string.IsNullOrEmpty(pass)) {
->>>>>>> parent of 75f875d... Database Created
                 // send error / account credentials not found
             }
 
-            session.AccountId = AccountStorage.DEFAULT_ACCOUNT_ID;
+            session.AccountId = User.Account_ID;
 
             switch (mode)
             {
