44796b66e12e9e1b95b9ddf854a9b6d5b626c846
Sparkmod <rafadagher@gmail.com>
2021-01-06 13:22:08 -0400

    Add: Skill Packet & Handle.
    - Added SkillPacket ( SKILL_USE & SKILL_DAMAGE)
    - Added SkillHandle.
    
    Still working on how mobs are spawn and deducting the damage is done by the skill, because mobs are'nt modifying any stats and they need to be pass on the field, so not sure on how to test this without that.

---
+++ MapleServer2/PacketHandlers/Game/SkillHandler.cs
@@ -0,0 +1,127 @@
+﻿using Maple2Storage.Types;
+using MaplePacketLib2.Tools;
+using MapleServer2.Constants;
+using MapleServer2.Packets;
+using MapleServer2.Servers.Game;
+using Microsoft.Extensions.Logging;
+
+namespace MapleServer2.PacketHandlers.Game
+{
+    public class SkillHandler : GamePacketHandler
+    {
+        public override RecvOp OpCode => RecvOp.SKILL;
+
+        public SkillHandler(ILogger<SkillHandler> logger) : base(logger) { }
+
+        private enum SkillHandlerMode : byte
+        {
+            FirstSent = 0x0,  // Start of a skill
+            Damage = 0x1,     // Damaging part of a skill. one is sent per hit
+            Mode3 = 0x3,
+            Mode4 = 0x4
+        }
+
+        private enum DamagingMode : byte
+        {
+            TypeOfDamage = 0x0,
+            AoeDamage = 0x1,
+            TypeOfDamage2 = 0x2
+        }
+
+        public override void Handle(GameSession session, PacketReader packet)
+        {
+            short skillLevel;
+            long skillCount;
+            int skillId;
+            int value;
+            int playerObjectId;
+            CoordF coords;
+            CoordF coords2;
+            CoordF coords3;
+
+            SkillHandlerMode mode = (SkillHandlerMode)packet.ReadByte();
+            switch (mode)
+            {
+                case SkillHandlerMode.FirstSent:
+                    skillCount = packet.ReadLong();
+                    value = packet.ReadInt();
+                    skillId = packet.ReadInt();
+                    skillLevel = packet.ReadShort();
+                    packet.ReadByte();
+                    coords = packet.Read<CoordF>();
+                    packet.ReadShort();
+                    session.Send(SkillUsePacket.SkillUse(value, skillCount, skillId, skillLevel, coords));
+                    break;
+                case SkillHandlerMode.Damage:
+                    DamagingMode type = (DamagingMode)packet.ReadByte();
+                    switch (type)
+                    {
+                        case DamagingMode.TypeOfDamage:
+                            skillCount = packet.ReadLong();
+                            packet.ReadByte();
+                            coords = packet.Read<CoordF>();
+                            coords2 = packet.Read<CoordF>();
+                            int count2 = packet.ReadByte();
+                            packet.ReadInt();
+                            for (int i = 0; i < count2; i++)
+                            {
+                                packet.ReadLong();
+                                packet.ReadInt();
+                                packet.ReadByte();
+                                if (packet.ReadBool())
+                                {
+                                    packet.ReadLong();
+                                }
+                            }
+                            break;
+                        case DamagingMode.AoeDamage:
+                            skillCount = packet.ReadLong();
+                            int someValue = packet.ReadInt();
+                            playerObjectId = packet.ReadInt();
+                            coords = packet.Read<CoordF>();
+                            coords2 = packet.Read<CoordF>();
+                            coords3 = packet.Read<CoordF>();
+                            packet.ReadByte();
+                            byte count3 = packet.ReadByte();
+                            packet.ReadInt();
+                            /*for (int i = 0; i < count3; i++)
+                            {
+                                EntityNpc.Add(session.FieldNpc);
+                                packet.ReadByte();
+                            }
+                            if (EntityNpc.Count > 0)
+                            {
+                                logger.LogInformation($"entities: "+ EntityNpc.Count);
+                            }*/
+                            int objectId = packet.ReadInt();
+                            packet.ReadByte();
+
+                            // Hardcoded SkillId(bow), SkillLeve(1)
+                            session.Send(SkillUsePacket.SkillDamage(session.FieldPlayer, skillCount, someValue, 600001, 1, coords, objectId));
+                            break;
+                        case DamagingMode.TypeOfDamage2:
+                            skillCount = packet.ReadLong();
+                            packet.ReadByte();
+                            packet.ReadInt();
+                            packet.ReadInt();
+                            packet.Read<CoordF>();
+                            packet.Read<CoordF>();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case SkillHandlerMode.Mode3:
+                    packet.ReadLong();
+                    packet.ReadInt();
+                    break;
+                case SkillHandlerMode.Mode4:
+                    packet.ReadLong();
+                    break;
+                default:
+                    IPacketHandler<GameSession>.LogUnknownMode(mode);
+                    break;
+            }
+        }
+    }
+}
---
+++ MapleServer2/Packets/SkillUsePacket.cs
@@ -0,0 +1,52 @@
+﻿using Maple2Storage.Types;
+using MaplePacketLib2.Tools;
+using MapleServer2.Constants;
+using MapleServer2.Types;
+
+namespace MapleServer2.Packets
+{
+    public static class SkillUsePacket
+    {
+        public static Packet SkillUse(int value, long count, int skillId, short skillLevel, CoordF coords)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.SKILL_USE);
+            pWriter.WriteLong(count);
+            pWriter.WriteInt(value);    // Unknown
+            pWriter.WriteInt(skillId);
+            pWriter.WriteShort(skillLevel);
+            pWriter.WriteByte();
+            pWriter.Write(coords);
+            pWriter.WriteLong();
+            pWriter.WriteLong();
+            pWriter.WriteInt();
+            pWriter.WriteShort();
+            pWriter.WriteInt();
+            pWriter.WriteByte();
+            pWriter.WriteByte();
+            return pWriter;
+        }
+
+        public static Packet SkillDamage(IFieldObject<Player> player, long skillCount, int someValue, int skillId, short skillLevel, CoordF coords, int objectId)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.SKILL_DAMAGE);
+            pWriter.WriteByte(1);
+            pWriter.WriteLong(skillCount);
+            pWriter.WriteInt(someValue);
+            pWriter.WriteInt(player.ObjectId);
+            pWriter.WriteInt(player.ObjectId);
+            pWriter.WriteInt(skillId);
+            pWriter.WriteShort(skillLevel);
+            pWriter.WriteShort(1);
+            pWriter.Write(coords);
+            pWriter.WriteInt();
+            pWriter.WriteByte(9); // Heal or Damage
+            pWriter.WriteByte(1); //
+            pWriter.WriteByte(1); // How many mobs where hit
+            pWriter.WriteInt(objectId);
+            pWriter.WriteByte(1);
+            pWriter.WriteBool(false);
+            pWriter.WriteLong(-1 * 1); // Probably damage deduct
+            return pWriter;
+        }
+    }
+}
