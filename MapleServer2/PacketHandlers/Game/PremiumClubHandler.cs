4b07f5f4891a4a06fcced222ec8cc665bfc98eba
Zin <62830952+Zintixx@users.noreply.github.com>
2021-01-10 20:45:15 -0800

    Premium Club packets

---
+++ MapleServer2/PacketHandlers/Game/PremiumClubHandler.cs
@@ -0,0 +1,73 @@
+﻿using MaplePacketLib2.Tools;
+using MapleServer2.Constants;
+using MapleServer2.Packets;
+using MapleServer2.Servers.Game;
+using Microsoft.Extensions.Logging;
+
+namespace MapleServer2.PacketHandlers.Game
+{
+    public class PremiumClubHandler : GamePacketHandler
+    {
+        public override RecvOp OpCode => RecvOp.VIP_GOODS_SHOP;
+
+        public PremiumClubHandler(ILogger<ClubHandler> logger) : base(logger) { }
+
+        private enum PremiumClubMode : byte
+        {
+            OpenUI = 0x1,
+            ClaimItems = 0x2,
+            PurchaseWindow = 0x3,
+            PurchaseMembership = 0x4,
+        }
+
+        public override void Handle(GameSession session, PacketReader packet)
+        {
+            PremiumClubMode mode = (PremiumClubMode) packet.ReadByte();
+
+            switch (mode)
+            {
+                case PremiumClubMode.OpenUI:
+                    HandleOpenUI(session, packet);
+                    break;
+                case PremiumClubMode.ClaimItems:
+                    HandleClaimItems(session, packet);
+                    break;
+                case PremiumClubMode.PurchaseWindow:
+                    HandlePurchaseWindow(session, packet);
+                    break;
+                case PremiumClubMode.PurchaseMembership:
+                    HandlePurchaseMembership(session, packet);
+                    break;
+                default:
+                    IPacketHandler<GameSession>.LogUnknownMode(mode);
+                    break;
+            }
+        }
+
+        private void HandleOpenUI(GameSession session, PacketReader packet)
+        {
+            session.Send(PremiumClubPacket.OpenUIConfirm());
+        }
+
+        private void HandleClaimItems(GameSession session, PacketReader packet)
+        {
+            int benefitId = packet.ReadInt();
+            session.Send(PremiumClubPacket.ClaimItemConfirm(benefitId));
+            // TODO grab data from \table\vipbenefititemtable.xml for item ID, quantity, rank
+            // TODO only claim once a day
+        }
+
+        private void HandlePurchaseWindow(GameSession session, PacketReader packet)
+        {
+            session.Send(PremiumClubPacket.PurchaseWindowConfirm());
+        }
+
+        private void HandlePurchaseMembership(GameSession session, PacketReader packet)
+        {
+            int packageId = packet.ReadInt();
+            session.Send(PremiumClubPacket.PurchaseMembershipConfirm(packageId));
+            // TODO grab data from \table\vipgoodstable.xml for pricing, buff duration, and bonus items
+            session.Send(PremiumClubPacket.ActivatePremium(session));
+        }
+    }
+}
---
+++ MapleServer2/Packets/PremiumClubPacket.cs
@@ -0,0 +1,59 @@
+﻿using MaplePacketLib2.Tools;
+using MapleServer2.Constants;
+using MapleServer2.Servers.Game;
+
+namespace MapleServer2.Packets
+{
+    public static class PremiumClubPacket
+    {
+        private enum PremiumClubPacketMode : byte
+        {
+            ActivatePremium = 0x0,
+            OpenUIConfirm = 0x1,
+            ClaimItemConfirm = 0x2,
+            PurchaseWindowConfirm = 0x3,
+            PurchaseMembershipConfirm = 0x4,
+        }
+
+        public static Packet OpenUIConfirm()
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.VIP_GOODS_SHOP);
+            pWriter.WriteMode(PremiumClubPacketMode.OpenUIConfirm);
+            pWriter.WriteInt();
+            return pWriter;
+        }
+
+        public static Packet ClaimItemConfirm(int benefitId)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.VIP_GOODS_SHOP);
+            pWriter.WriteMode(PremiumClubPacketMode.ClaimItemConfirm);
+            pWriter.WriteInt(benefitId);
+            return pWriter;
+        }
+
+        public static Packet PurchaseWindowConfirm()
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.VIP_GOODS_SHOP);
+            pWriter.WriteMode(PremiumClubPacketMode.PurchaseWindowConfirm);
+            pWriter.WriteInt();
+            return pWriter;
+        }
+
+        public static Packet PurchaseMembershipConfirm(int packageId)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.VIP_GOODS_SHOP);
+            pWriter.WriteMode(PremiumClubPacketMode.PurchaseMembershipConfirm);
+            pWriter.WriteInt(packageId);
+            return pWriter;
+        }
+
+        public static Packet ActivatePremium(GameSession session)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.VIP_GOODS_SHOP);
+            pWriter.WriteMode(PremiumClubPacketMode.ActivatePremium);
+            pWriter.WriteInt(session.FieldPlayer.ObjectId);
+            pWriter.WriteLong(2592847227); // expiration timestamp
+            return pWriter;
+        }
+    }
+}
