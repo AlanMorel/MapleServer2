3041e8ed48a6bc48a9335dbe1da473ee5c069aad
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2021-01-11 22:46:45 -0300

    Parser for Insiginia added

--- GameDataParser/Program.cs
+++ GameDataParser/Program.cs
@@ -12,12 +12,14 @@ namespace GameDataParser
         public static ItemMetadataExport Item = new ItemMetadataExport();
         public static MapMetadataExport MapEntity = new MapMetadataExport();
         public static SkillMetadataExport Skill = new SkillMetadataExport();
+        public static InsigniaMetadataExport Insignia = new InsigniaMetadataExport();
 
         private static void Main()
         {
             Item.Export();
             MapEntity.Export();
             Skill.Export();
+            Insignia.Export();
         }
 
         public static XmlReader GetReader(this MemoryMappedFile m2dFile, IPackFileHeader header)
