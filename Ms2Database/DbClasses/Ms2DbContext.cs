17bf65a5c619e3f4e9e0928f404cb9a3e0dc8b78
bwu113 <bowenwu112@gmail.com>
2021-01-14 23:19:24 -0500

    Update Ms2DbContext.cs

--- Ms2Database/DbClasses/Ms2DbContext.cs
+++ Ms2Database/DbClasses/Ms2DbContext.cs
@@ -7,14 +7,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Ms2Database.DbClasses
 {
-    public class Ms2DbContext : DbContext
+    public class Ms2DbContext : DbContext // Create Database Schema
     {
         public DbSet<Account> Accounts { get; set; }
         public DbSet<Character> Characters { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB; Database=Maplestory2DB; Trusted_Connection=True;");
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB; Database=Maplestory2DB; Trusted_Connection=True;"); // Connection String
         }
     }
 }
