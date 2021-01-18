18bc50c97ce7bc6d6135db71348541d5f29d6085
Sparkmod <rafadagher@gmail.com>
2021-01-18 04:09:58 -0400

    Adding SubSkills on parsing
    - Parsing Subskill on each Skill that has a sub skill

--- GameDataParser/Parsers/SkillParser.cs
+++ GameDataParser/Parsers/SkillParser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.IO.MemoryMappedFiles;
+using System.Linq;
 using System.Xml;
 using GameDataParser.Crypto.Common;
 using GameDataParser.Files;
@@ -13,88 +14,121 @@ namespace GameDataParser.Parsers
 {
     public static class SkillParser
     {
-
         public static List<SkillMetadata> Parse(MemoryMappedFile m2dFile, IEnumerable<PackFileEntry> entries)
         {
             List<SkillMetadata> skillList = new List<SkillMetadata>();
-
-            foreach (PackFileEntry skill in entries)
+            List<ListSubSkill> subSkillList = new List<ListSubSkill>();
+            foreach (PackFileEntry xmlFile in entries)
             {
-                if (!skill.Name.StartsWith("skill"))
-                    continue;
-
-                string skillId = Path.GetFileNameWithoutExtension(skill.Name);
-                SkillMetadata metadata = new SkillMetadata();
-                List<SkillLevel> skillLevel = new List<SkillLevel>();
-
-                metadata.SkillId = int.Parse(skillId);
-                Debug.Assert(metadata.SkillId > 0, $"Invalid Id {metadata.SkillId} from {skillId}");
-
-                //using XmlReader reader = m2dFile.GetReader(skill.FileHeader);
-                XmlDocument document = m2dFile.GetDocument(skill.FileHeader);
-
-                XmlNodeList basic = document.SelectNodes("/ms2/basic");
-                foreach (XmlNode node in basic)
+                // Parsing Skills
+                if (xmlFile.Name.StartsWith("skill"))
                 {
-                    if (node.Attributes.GetNamedItem("feature") != null)
+                    string skillId = Path.GetFileNameWithoutExtension(xmlFile.Name);
+                    SkillMetadata metadata = new SkillMetadata();
+                    List<SkillLevel> skillLevel = new List<SkillLevel>();
+
+                    metadata.SkillId = int.Parse(skillId);
+                    Debug.Assert(metadata.SkillId > 0, $"Invalid Id {metadata.SkillId} from {skillId}");
+
+                    XmlDocument document = m2dFile.GetDocument(xmlFile.FileHeader);
+
+                    XmlNodeList basic = document.SelectNodes("/ms2/basic");
+                    foreach (XmlNode node in basic)
                     {
-                        // Get Rank
-                        metadata.Feature = node.Attributes["feature"].Value;
+                        if (node.Attributes.GetNamedItem("feature") != null)
+                        {
+                            // Get Rank
+                            metadata.Feature = node.Attributes["feature"].Value;
+                        }
                     }
-                    XmlNode kinds = node.SelectSingleNode("kinds");
-                    metadata.Type = kinds.Attributes["type"].Value != null ? int.Parse(kinds.Attributes["type"].Value) : 0;
-                    if (kinds.Attributes.GetNamedItem("subType") != null)
+
+                    XmlNodeList levels = document.SelectNodes("/ms2/level");
+                    foreach (XmlNode node in levels)
                     {
-                        // Get Rank
-                        metadata.SubType = kinds.Attributes["subType"].Value != null ? int.Parse(kinds.Attributes["subType"].Value) : 0;
+                        if (node.Attributes.GetNamedItem("feature") == null || node.Attributes.GetNamedItem("feature").Value == metadata.Feature)
+                        {
+                            int level = node.Attributes["value"].Value != null ? int.Parse(node.Attributes["value"].Value) : 1;
+                            XmlNode consume = node.SelectSingleNode("consume/stat");
+
+                            int spirit = 0;
+                            int upgradeLevel = 0;
+                            int[] upgradeSkillId = new int[0];
+                            int[] upgradeSkillLevel = new int[0];
+
+                            if (consume.Attributes.GetNamedItem("sp") != null)
+                            {
+                                spirit = int.Parse(consume.Attributes["sp"].Value);
+                            }
+
+                            XmlNode upgrade = node.SelectSingleNode("upgrade");
+                            if (upgrade.Attributes != null)
+                            {
+                                if (upgrade.Attributes.GetNamedItem("level") != null)
+                                {
+                                    upgradeLevel = int.Parse(upgrade.Attributes["level"].Value);
+                                }
+                                if (upgrade.Attributes.GetNamedItem("skillIDs") != null)
+                                {
+                                    upgradeSkillId = Array.ConvertAll(upgrade.Attributes.GetNamedItem("skillIDs").Value.Split(","), int.Parse);
+                                }
+                                if (upgrade.Attributes.GetNamedItem("skillLevels") != null)
+                                {
+                                    upgradeSkillLevel = Array.ConvertAll(upgrade.Attributes.GetNamedItem("skillLevels").Value.Split(","), int.Parse);
+                                }
+                            }
+                            skillLevel.Add(new SkillLevel(level, spirit, upgradeLevel, upgradeSkillId, upgradeSkillLevel));
+                            metadata.SkillLevel = skillLevel[0];
+                        }
                     }
-                    XmlNode stateAttr = node.SelectSingleNode("stateAttr");
-                    if (stateAttr.Attributes.GetNamedItem("useDefaultSkill") != null)
+                    skillList.Add(metadata);
+                }
+                // Parsing SubSkills
+                else if (xmlFile.Name.StartsWith("table/job"))
+                {
+                    XmlDocument document = m2dFile.GetDocument(xmlFile.FileHeader);
+                    XmlNodeList job = document.SelectNodes("/ms2/job");
+                    foreach (XmlNode node in job)
                     {
-                        // Get Rank
-                        metadata.DefaultSkill = stateAttr.Attributes["useDefaultSkill"].Value != null ? byte.Parse(stateAttr.Attributes["useDefaultSkill"].Value) : 0;
+                        if (node.Attributes.GetNamedItem("feature") != null)
+                        {
+                            string feature = node.Attributes["feature"].Value;
+                            if (feature == "JobChange_02")
+                            {
+                                XmlNode skills = node.SelectSingleNode("skills");
+                                for (int i = 0; i < skills.ChildNodes.Count; i++)
+                                {
+                                    int id = int.Parse(skills.ChildNodes[i].Attributes["main"].Value);
+                                    int[] sub = new int[0];
+                                    if (skills.ChildNodes[i].Attributes["sub"] != null)
+                                    {
+                                        if (skillList.Select(x => x.SkillId).Contains(id))
+                                        {
+                                            sub = Array.ConvertAll(skills.ChildNodes[i].Attributes["sub"].Value.Split(","), int.Parse);
+                                            skillList.Find(x => x.SkillId == id).SubSkill = sub;
+                                        }
+                                    }
+                                    subSkillList.Add(new ListSubSkill(id, sub));
+                                }
+                            }
+                        }
+                        else if (node.Attributes.GetNamedItem("code").Value == "001")
+                        {
+                            XmlNode skills = node.SelectSingleNode("skills");
+
+                            for (int i = 0; i < skills.ChildNodes.Count; i++)
+                            {
+                                int id = int.Parse(skills.ChildNodes[i].Attributes["main"].Value);
+                                int[] sub = new int[0];
+                                if (skills.ChildNodes[i].Attributes["sub"] != null)
+                                {
+                                    sub = Array.ConvertAll(skills.ChildNodes[i].Attributes["sub"].Value.Split(","), int.Parse);
+                                }
+                                subSkillList.Add(new ListSubSkill(id, sub));
+                            }
+                        }
                     }
                 }
-
-                XmlNodeList levels = document.SelectNodes("/ms2/level");
-                foreach (XmlNode node in levels)
-                {
-                    if (node.Attributes.GetNamedItem("feature") == null || node.Attributes.GetNamedItem("feature").Value == metadata.Feature)
-                    {
-                        int level = node.Attributes["value"].Value != null ? int.Parse(node.Attributes["value"].Value) : 1;
-                        XmlNode consume = node.SelectSingleNode("consume/stat");
-
-                        int spirit = 0;
-                        int upgradeLevel = 0;
-                        int[] upgradeSkillId = new int[0];
-                        int[] upgradeSkillLevel = new int[0];
-
-                        if (consume.Attributes.GetNamedItem("sp") != null)
-                        {
-                            spirit = int.Parse(consume.Attributes["sp"].Value);
-                        }
-
-                        XmlNode upgrade = node.SelectSingleNode("upgrade");
-                        if (upgrade.Attributes != null)
-                        {
-                            if (upgrade.Attributes.GetNamedItem("level") != null)
-                            {
-                                upgradeLevel = int.Parse(upgrade.Attributes["level"].Value);
-                            }
-                            if (upgrade.Attributes.GetNamedItem("skillIDs") != null)
-                            {
-                                upgradeSkillId = Array.ConvertAll(upgrade.Attributes.GetNamedItem("skillIDs").Value.Split(","), int.Parse);
-                            }
-                            if (upgrade.Attributes.GetNamedItem("skillLevels") != null)
-                            {
-                                upgradeSkillLevel = Array.ConvertAll(upgrade.Attributes.GetNamedItem("skillLevels").Value.Split(","), int.Parse);
-                            }
-                        }
-                        skillLevel.Add(new SkillLevel(level, spirit, upgradeLevel, upgradeSkillId, upgradeSkillLevel));
-                        metadata.SkillLevel = skillLevel[0];
-                    }
-                }
-                skillList.Add(metadata);
+                // Add SubSkills to Skills
             }
             return skillList;
         }
--- Maple2Storage/Types/Metadata/SkillMetadata.cs
+++ Maple2Storage/Types/Metadata/SkillMetadata.cs
@@ -13,72 +13,24 @@ namespace Maple2Storage.Types.Metadata
         [XmlElement(Order = 2)]
         public string Feature = "";
         [XmlElement(Order = 3)]
-        public int Type;
-        [XmlElement(Order = 4)]
-        public int SubType;
-        [XmlElement(Order = 5)]
-        public byte DefaultSkill;
-        [XmlElement(Order = 6)]
         public SkillLevel SkillLevel;
-        [XmlElement(Order = 7)]
+        [XmlElement(Order = 4)]
+        public int[] SubSkill = new int[0];
+        [XmlElement(Order = 5)]
         public byte Learned = 0;
 
         public SkillMetadata()
         {
-            this.SkillLevel = new SkillLevel();
-        }
-
-        public SkillMetadata(int skillId)
-        {
-            this.SkillId = skillId;
-            this.SkillLevel = new SkillLevel();
-        }
-
-        public SkillMetadata(int skillId, byte learned, SkillLevel skillLevel)
-        {
-            this.SkillId = skillId;
-            this.SkillLevel = skillLevel;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (obj is null)
-                return false;
-            if (ReferenceEquals(this, obj))
-                return true;
-            if (obj.GetType() != this.GetType())
-                return false;
-            return Equals((SkillMetadata) obj);
-        }
-
-        protected bool Equals(SkillMetadata other)
-        {
-            return SkillId == other.SkillId
-                && Feature.Equals(other.Feature)
-                && Type.Equals(other.Type)
-                && SubType.Equals(other.SubType)
-                && DefaultSkill.Equals(other.DefaultSkill)
-                && SkillLevel.Equals(other.SkillLevel)
-                && Learned.Equals(other.Learned);
+            SkillLevel = new SkillLevel();
         }
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(SkillId, Feature, Type, SubType, SkillLevel, Learned);
-        }
-
-        public static bool operator ==(SkillMetadata left, SkillMetadata right)
-        {
-            return Equals(left, right);
-        }
-
-        public static bool operator !=(SkillMetadata left, SkillMetadata right)
-        {
-            return !Equals(left, right);
+            return HashCode.Combine(SkillId, Feature, SkillLevel, Learned);
         }
 
         public override string ToString() =>
-            $"Skill:(Id:{SkillId},Feature:{Feature},Type:{Type},SubType:{SubType},Default:{DefaultSkill},SkillLevel:{string.Join(",", SkillLevel)},Learned:{Learned})";
+            $"Skill:(Id:{SkillId},Feature:{Feature},SkillLevel:{string.Join(",", SkillLevel)},Learned:{Learned}";
     }
 
     [XmlType]
@@ -103,31 +55,11 @@ namespace Maple2Storage.Types.Metadata
 
         public SkillLevel(int _Level, int _Spirit, int _UpgradeLevel, int[] _UpgradeSkillId, int[] _UpgradeSkillLevel)
         {
-            this.Level = _Level;
-            this.Spirit = _Spirit;
-            this.UpgradeLevel = _UpgradeLevel;
-            this.UpgradeSkillId = _UpgradeSkillId;
-            this.UpgradeSkillLevel = _UpgradeSkillLevel;
-        }
-
-        protected bool Equals(SkillLevel other)
-        {
-            return Level == other.Level
-                && Spirit.Equals(other.Spirit)
-                && UpgradeLevel.Equals(other.UpgradeLevel)
-                && UpgradeSkillId.Equals(other.UpgradeSkillId)
-                && UpgradeSkillLevel.Equals(other.UpgradeSkillLevel);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj))
-                return false;
-            if (ReferenceEquals(this, obj))
-                return true;
-            if (obj.GetType() != this.GetType())
-                return false;
-            return Equals((SkillLevel) obj);
+            Level = _Level;
+            Spirit = _Spirit;
+            UpgradeLevel = _UpgradeLevel;
+            UpgradeSkillId = _UpgradeSkillId;
+            UpgradeSkillLevel = _UpgradeSkillLevel;
         }
 
         public override int GetHashCode()
@@ -135,17 +67,29 @@ namespace Maple2Storage.Types.Metadata
             return HashCode.Combine(Level, Spirit, UpgradeLevel, UpgradeSkillId, UpgradeSkillLevel);
         }
 
-        public static bool operator ==(SkillLevel left, SkillLevel right)
-        {
-            return Equals(left, right);
-        }
-
-        public static bool operator !=(SkillLevel left, SkillLevel right)
-        {
-            return !Equals(left, right);
-        }
-
         public override string ToString() =>
             $"SkillLevel(Level:{Level},Spirit:{Spirit},UpgradeLevel:{UpgradeLevel},SkillRequired:{UpgradeSkillId},SkillLevelRequired:{UpgradeSkillLevel})";
+    }
+
+    [XmlType]
+    public class ListSubSkill
+    {
+        [XmlElement(Order = 1)]
+        public int Id { get; set; }
+        [XmlElement(Order = 2)]
+        public int[] Sub { get; set; }
+
+        public ListSubSkill()
+        {
+
+        }
+
+        public ListSubSkill(int id, int[] sub)
+        {
+            Id = id;
+            Sub = sub;
+        }
+
+        public override string ToString() => $"Skill(Id:{Id},Sub:{string.Join(",", Sub)})";
     }
 }
