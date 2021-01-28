813602f35d31f5b68dd720586939bd616cae2577
Sparkmod <rafadagher@gmail.com>
2021-01-28 18:59:17 -0400

    Fix: Write passive skills correctly (#155)
    - Remove hardcoded passiveskill. It was passing the same skill `105000111` on every job
    - Parsing `Type` into skillmetadata. `Passive = 1 - Active  = 0` `Item = 3`
    - Added `WritePassiveSkills` for each job
    - Added `SkillMotion` into SkillMetadata for RegionEffects

--- GameDataParser/Parsers/SkillParser.cs
+++ GameDataParser/Parsers/SkillParser.cs
@@ -24,18 +24,25 @@ namespace GameDataParser.Parsers
                     string skillId = Path.GetFileNameWithoutExtension(entry.Name);
                     SkillMetadata metadata = new SkillMetadata();
                     List<SkillLevel> skillLevels = new List<SkillLevel>();
-
                     metadata.SkillId = int.Parse(skillId);
                     XmlDocument document = resources.xmlMemFile.GetDocument(entry.FileHeader);
+
+                    metadata.Type = byte.Parse(document.SelectSingleNode("/ms2/basic/kinds").Attributes["type"].Value);
+
                     XmlNodeList levels = document.SelectNodes("/ms2/level");
                     foreach (XmlNode level in levels)
                     {
                         // Getting all skills level
+                        XmlNode motionProperty = level.SelectSingleNode("motion/motionProperty");
                         string feature = level.Attributes["feature"] != null ? level.Attributes["feature"].Value : "";
                         int levelValue = level.Attributes["value"].Value != null ? int.Parse(level.Attributes["value"].Value) : 0;
                         int spirit = level.SelectSingleNode("consume/stat").Attributes["sp"] != null ? int.Parse(level.SelectSingleNode("consume/stat").Attributes["sp"].Value) : 0;
                         float damageRate = level.SelectSingleNode("motion/attack/damageProperty") != null ? float.Parse(level.SelectSingleNode("motion/attack/damageProperty").Attributes["rate"].Value) : 0;
-                        skillLevels.Add(new SkillLevel(levelValue, spirit, damageRate, feature));
+                        string sequenceName = motionProperty != null ? motionProperty.Attributes["sequenceName"].Value : "";
+                        string motionEffect = motionProperty != null ? motionProperty.Attributes["motionEffect"].Value : "";
+
+                        SkillMotion skillMotion = new SkillMotion(sequenceName, motionEffect);
+                        skillLevels.Add(new SkillLevel(levelValue, spirit, damageRate, feature, skillMotion));
                     }
                     metadata.SkillLevels = skillLevels;
                     string state = document.SelectSingleNode("/ms2/basic/kinds").Attributes["state"]?.Value;
