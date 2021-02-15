17d69c9f26d8b72aad9caa937e536965758ad6bc
voodoonofx <voodoonofx@gmail.com>
2021-02-14 19:41:24 -0800

    FIX: HandlePickup was comparing an invalid type. Instantiated the correct type (CoordB). (#173)

--- MapleServer2/PacketHandlers/Game/RequestCubeHandler.cs
+++ MapleServer2/PacketHandlers/Game/RequestCubeHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Maple2Storage.Types;
 using Maple2Storage.Types.Metadata;
 using MaplePacketLib2.Tools;
 using MapleServer2.Constants;
@@ -48,7 +49,7 @@ namespace MapleServer2.PacketHandlers.Game
             // Find matching mapObject
             foreach (MapObject mapObject in MapEntityStorage.GetObjects(session.Player.MapId))
             {
-                if (mapObject.Coord.Equals(sCoords))
+                if (mapObject.Coord.Equals(CoordB.From(sCoords[0], sCoords[1], sCoords[2])))
                 {
                     weaponId = mapObject.WeaponId;
                     break;
