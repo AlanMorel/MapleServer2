964dc020cb0d6aed6174ce3e96b7b87ef91b3ba5
Wevvin <4209227+wevvin@users.noreply.github.com>
2021-02-17 21:56:58 +1300

    no message

---
+++ MapleServer2/PacketHandlers/Game/ShopHandler.cs
@@ -0,0 +1,44 @@
+﻿using MaplePacketLib2.Tools;
+using MapleServer2.Constants;
+using MapleServer2.Servers.Game;
+using MapleServer2.Types;
+using Microsoft.Extensions.Logging;
+
+namespace MapleServer2.PacketHandlers.Game
+{
+    public class ShopHandler : GamePacketHandler
+    {
+        
+        public override RecvOp OpCode => RecvOp.SHOP;
+        
+        public ShopHandler(ILogger<ShopHandler> logger) : base(logger) { }
+
+        private enum ShopMode : byte
+        {
+            Open = 0x0,
+            GetProducts = 0x1,
+            Close = 0x6
+        }
+
+        public override void Handle(GameSession session, PacketReader packet)
+        {
+            ShopMode mode = (ShopMode) packet.ReadByte();
+
+            switch (mode)
+            {
+                default:
+                    IPacketHandler<GameSession>.LogUnknownMode(mode);
+                    break;
+            }
+        }
+
+        private static void HandleOpen(Player player, PacketReader packet) {
+            //Logger.LogInformation("Opening Shop ID %d;", packet);
+            // List<NpcShopProduct> products = shopData.get(npc.getTemplate().getShopID());
+            // user.sendPacket(FieldPacket.onSendShop(0, npc.getTemplateID(), null));
+            // user.sendPacket(FieldPacket.onSendShop(1, npc.getTemplateID(), products == null ? new ArrayList<NpcShopProduct>() : products));
+            // user.sendPacket(FieldPacket.onSendShop(6, npc.getTemplateID(), null));
+            // user.sendPacket(FieldPacket.onSendNpcTalk(npc.getEntityID(), 1, 0, NpcTalkFlag.NONE));
+        }
+    }
+}
---
+++ MapleServer2/Packets/ShopPacket.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using MaplePacketLib2.Tools;
+using MapleServer2.Constants;
+
+namespace MapleServer2.Packets
+{
+    public static class ShopPacket
+    {
+        private enum ShopMode : byte
+        {
+            Open = 0x0,
+            GetProducts = 0x1,
+            Close = 0x6
+        }
+        
+        public static Packet Open(int npcTemplateId, int shopCategory, string shopName)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.SHOP);
+
+            // Currently unknown
+            bool idk = false;
+            
+            pWriter.WriteInt(npcTemplateId);
+            pWriter.WriteInt(128);
+            pWriter.WriteLong(DateTimeOffset.Now.ToUnixTimeSeconds()); // current date/time in seconds
+            pWriter.WriteInt(0);
+            pWriter.WriteShort(3);
+            pWriter.WriteInt(shopCategory); // ShopCategory (916 = Juice)
+            pWriter.WriteByte(0);
+            pWriter.WriteBool(false);
+            pWriter.WriteBool(idk);
+            pWriter.WriteBool(false);
+            pWriter.WriteByte(0);
+            pWriter.WriteBool(true);
+            pWriter.WriteBool(false);
+            pWriter.WriteBool(false);
+            pWriter.WriteBool(false);
+            pWriter.WriteUnicodeString(shopName);
+            
+            if (idk) {
+                pWriter.WriteByte(0);
+                pWriter.WriteByte(0);
+                pWriter.WriteInt(0);
+                pWriter.WriteInt(0);
+                pWriter.WriteBool(false);
+                pWriter.WriteInt(0);
+                pWriter.WriteByte(0);
+                pWriter.WriteBool(false);
+                pWriter.WriteBool(false);
+            }
+
+            return pWriter;
+        }
+
+        public static Packet LoadProducts(List<NpcShopProduct> products)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.SHOP);
+            pWriter.WriteByte(products.size());
+            foreach (NpcShopProduct product in products)
+            {
+                pWriter.Write(product);
+            }
+        }
+
+        public static Packet Close()
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.SHOP);
+            pWriter.WriteShort(0);
+            return pWriter;
+        }
+    }
+}
