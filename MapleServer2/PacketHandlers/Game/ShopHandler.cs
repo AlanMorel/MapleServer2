91f44f23b794b3146a4ec57de193ead2f7d61b7f
Wevvin <james.rawlings22@gmail.com>
2021-02-18 19:07:22 +1300

    WIP

--- MapleServer2/PacketHandlers/Game/ShopHandler.cs
+++ MapleServer2/PacketHandlers/Game/ShopHandler.cs
@@ -1,23 +1,22 @@
-﻿using MaplePacketLib2.Tools;
+﻿using System;
+using MaplePacketLib2.Tools;
 using MapleServer2.Constants;
 using MapleServer2.Packets;
 using MapleServer2.Servers.Game;
-using MapleServer2.Types;
 using Microsoft.Extensions.Logging;
 
 namespace MapleServer2.PacketHandlers.Game
 {
     public class ShopHandler : GamePacketHandler
     {
-        
         public override RecvOp OpCode => RecvOp.SHOP;
-        
+
         public ShopHandler(ILogger<ShopHandler> logger) : base(logger) { }
 
         private enum ShopMode : byte
         {
             Open = 0x0,
-            GetProducts = 0x1,
+            LoadProducts = 0x1,
             Buy = 0x4,
             Sell = 0x5,
             Close = 0x6
@@ -29,19 +28,72 @@ namespace MapleServer2.PacketHandlers.Game
 
             switch (mode)
             {
+                case ShopMode.Open:
+                    HandleOpen(session, packet);
+                    break;
+                case ShopMode.Close:
+                    HandleClose(session);
+                    break;
+                case ShopMode.Buy:
+                    HandleBuy(session, packet);
+                    break;
+                case ShopMode.Sell:
+                    HandleSell(session, packet);
+                    break;
                 default:
                     IPacketHandler<GameSession>.LogUnknownMode(mode);
                     break;
             }
         }
 
-        private static void HandleOpen(Player player, PacketReader packet) {
+        private static void HandleOpen(GameSession session, PacketReader packet)
+        {
+            Console.WriteLine("Opening shop...");
+            Console.WriteLine($"NPC: {packet.ReadInt()}");
+
             //Logger.LogInformation("Opening Shop ID %d;", packet);
             // List<NpcShopProduct> products = shopData.get(npc.getTemplate().getShopID());
             // user.sendPacket(FieldPacket.onSendShop(0, npc.getTemplateID(), null));
             // user.sendPacket(FieldPacket.onSendShop(1, npc.getTemplateID(), products == null ? new ArrayList<NpcShopProduct>() : products));
             // user.sendPacket(FieldPacket.onSendShop(6, npc.getTemplateID(), null));
             // user.sendPacket(FieldPacket.onSendNpcTalk(npc.getEntityID(), 1, 0, NpcTalkFlag.NONE));
+        }
+
+        private static void HandleClose(GameSession session)
+        {
+            Console.WriteLine("Closing Shop");
+            session.Send(ShopPacket.Close());
+        }
+
+        private static void HandleLoadProducts(GameSession session, PacketReader packet)
+        {
+            // public static Packet LoadProducts(List<NpcShopProduct> products)
+            // {
+            //     PacketWriter pWriter = PacketWriter.Of(SendOp.SHOP);
+            //     pWriter.WriteByte(products.size());
+            //     foreach (NpcShopProduct product in products)
+            //     {
+            //         pWriter.Write(product);
+            //     }
+            // }
+        }
+
+        private static void HandleSell(GameSession session, PacketReader packet)
+        {
+            // sell to shop
+            long itemUid = packet.ReadLong();
+            int quantity = packet.ReadInt();
+
+            session.Send(ShopPacket.Sell(itemUid, quantity));
+        }
+
+        private static void HandleBuy(GameSession session, PacketReader packet)
+        {
+            // buy from shop
+            int itemId = packet.ReadInt();
+            int quantity = packet.ReadInt();
+
+            session.Send(ShopPacket.Buy(itemId, quantity));
         }
     }
 }
--- MapleServer2/Packets/ShopPacket.cs
+++ MapleServer2/Packets/ShopPacket.cs
@@ -7,80 +7,63 @@ namespace MapleServer2.Packets
 {
     public static class ShopPacket
     {
-        private enum ShopMode : byte
-        {
-            Open = 0x0,
-            GetProducts = 0x1,
-            Buy = 0x4,
-            Sell = 0x5,
-            Close = 0x6
-        }
-        
         public static Packet Open(int npcTemplateId, int shopCategory, string shopName)
         {
             PacketWriter pWriter = PacketWriter.Of(SendOp.SHOP);
 
             // Currently unknown
-            bool idk = false;
-            
+            const bool idk = false;
+
             pWriter.WriteInt(npcTemplateId);
             pWriter.WriteInt(128);
             pWriter.WriteLong(DateTimeOffset.Now.ToUnixTimeSeconds()); // current date/time in seconds
-            pWriter.WriteInt(0);
+            pWriter.WriteInt();
             pWriter.WriteShort(3);
             pWriter.WriteInt(shopCategory); // ShopCategory (916 = Juice)
-            pWriter.WriteByte(0);
+            pWriter.WriteByte();
             pWriter.WriteBool(false);
             pWriter.WriteBool(idk);
             pWriter.WriteBool(false);
-            pWriter.WriteByte(0);
+            pWriter.WriteByte();
             pWriter.WriteBool(true);
             pWriter.WriteBool(false);
             pWriter.WriteBool(false);
             pWriter.WriteBool(false);
             pWriter.WriteUnicodeString(shopName);
-            
-            if (idk) {
-                pWriter.WriteByte(0);
-                pWriter.WriteByte(0);
-                pWriter.WriteInt(0);
-                pWriter.WriteInt(0);
-                pWriter.WriteBool(false);
-                pWriter.WriteInt(0);
-                pWriter.WriteByte(0);
-                pWriter.WriteBool(false);
-                pWriter.WriteBool(false);
-            }
+
+            // if (idk) {
+            //     pWriter.WriteByte(0);
+            //     pWriter.WriteByte(0);
+            //     pWriter.WriteInt(0);
+            //     pWriter.WriteInt(0);
+            //     pWriter.WriteBool(false);
+            //     pWriter.WriteInt(0);
+            //     pWriter.WriteByte(0);
+            //     pWriter.WriteBool(false);
+            //     pWriter.WriteBool(false);
+            // }
 
             return pWriter;
         }
 
-        // public static Packet LoadProducts(List<NpcShopProduct> products)
-        // {
-        //     PacketWriter pWriter = PacketWriter.Of(SendOp.SHOP);
-        //     pWriter.WriteByte(products.size());
-        //     foreach (NpcShopProduct product in products)
-        //     {
-        //         pWriter.Write(product);
-        //     }
-        // }
-        
-        // private void onSendShop(InPacket packet) {
-        //     //[SendShop] 28 00 04 24 2D 31 01 05 00 00 00 - 5 is    qty
-        //     int mode = packet.decodeByte();
-        //     if (mode == 4) { //buy from shop
-        //         int id = packet.decodeInt();
-        //         int quantity = packet.decodeInt();
-        //         ShopHandler.handleBuy(this, id, quantity);
-        //     } else if (mode == 5) { //sell to shop
-        //         long itemSN = packet.decodeLong();
-        //         int quantity = packet.decodeInt();
-        //         ShopHandler.handleSell(this, itemSN, quantity);
-        //     }
-        // }
-
         public static Packet Close()
         {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.SHOP);
+            pWriter.WriteShort(0);
+            return pWriter;
+        }
+
+        public static Packet Buy(long itemUid, int quantity)
+        {
+            // TODO: Implement buying item from shop
+            PacketWriter pWriter = PacketWriter.Of(SendOp.SHOP);
+            pWriter.WriteShort(0);
+            return pWriter;
+        }
+
+        public static Packet Sell(long itemId, int quantity)
+        {
+            // TODO: Implement selling item to shop
             PacketWriter pWriter = PacketWriter.Of(SendOp.SHOP);
             pWriter.WriteShort(0);
             return pWriter;
