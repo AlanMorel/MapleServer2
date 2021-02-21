9483f67c0c291145fd46ba09451bb358f10587bc
Zin <adrian.cs101@gmail.com>
2021-02-21 15:24:54 -0800

    Update RequestItemUseHandler.cs

--- MapleServer2/PacketHandlers/Game/RequestItemUseHandler.cs
+++ MapleServer2/PacketHandlers/Game/RequestItemUseHandler.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using MaplePacketLib2.Tools;
+﻿using MaplePacketLib2.Tools;
 using MapleServer2.Constants;
 using MapleServer2.PacketHandlers.Game.Helpers;
 using MapleServer2.Packets;
