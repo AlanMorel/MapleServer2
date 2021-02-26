138f3171b2e3206ce47c592af4c60820c6d4d63a
Wevvin <james.rawlings22@gmail.com>
2021-02-26 23:21:26 +1300

    WIP

---
+++ GameDataParser/Parsers/ShopParser.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using GameDataParser.Files;
+using Maple2Storage.Types;
+using Maple2Storage.Types.Metadata;
+
+namespace GameDataParser.Parsers
+{
+    public class ShopParser : Exporter<List<ShopMetadata>>
+    {
+        public ShopParser() : base(null, "shop") { }
+
+        protected override List<ShopMetadata> Parse()
+        {
+            List<ShopMetadata> shops = new List<ShopMetadata>();
+
+            /*
+             * Shop Name: Rumi
+             * Map: Lith Harbor
+             */
+            ShopMetadata rumi = new ShopMetadata()
+            {
+                TemplateId = 11000079,
+                Id = 103,
+                Category = 3,
+                Name = "shop",
+                RestrictSales = false,
+                CanRestock = false,
+                NextRestock = DateTimeOffset.UtcNow.ToUnixTimeSeconds(),
+                AllowBuyback = true,
+                Items = new List<ShopItem>
+                {
+                    new ShopItem
+                    {
+                        UniqueId = 400000,
+                        ItemId = 30000272,
+                        TokenType = Currency.Meso,
+                        Price = 500,
+                        SalePrice = 1000,
+                        ItemRank = 1,
+                        Quantity = 1,
+                        Category = "ETC",
+                        Flag = ShopItemFlag.HalfPrice
+                    }
+                }
+            };
+
+            shops.Add(rumi);
+
+            return shops;
+        }
+    }
+}
---
+++ Maple2Storage/Types/Currency.cs
@@ -0,0 +1,17 @@
+﻿namespace Maple2Storage.Types
+{
+    public enum Currency : byte
+    {
+        Capsule = 1,
+        Meso = 2,
+        ValorToken = 3,
+        Treva = 4,
+        Rue = 5,
+        HaviFruit = 6,
+        Meret = 7,
+        GameMeret = 8,
+        EventMeret = 9,
+        MesoToken = 10,
+        Bank = 11
+    }
+}
