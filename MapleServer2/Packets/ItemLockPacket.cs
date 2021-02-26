e2334002b2cb0617bf82c440e9cb331418ed6761
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2021-02-25 21:11:13 -0300

    Feat: Item lock (#187)
    * Feat: Item lock
    
    * Dotnet-format

---
+++ MapleServer2/Packets/ItemLockPacket.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using MaplePacketLib2.Tools;
+using MapleServer2.Constants;
+using MapleServer2.Packets.Helpers;
+using MapleServer2.Types;
+
+namespace MapleServer2.Packets
+{
+    public static class ItemLockPacket
+    {
+        private enum ItemLockMode : byte
+        {
+            Add = 0x01,
+            Remove = 0x02,
+            Update = 0x03,
+        }
+
+        public static Packet Add(long uid, short slot)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.ITEM_LOCK);
+            pWriter.WriteEnum(ItemLockMode.Add);
+            pWriter.WriteLong(uid);
+            pWriter.WriteShort(slot);
+
+            return pWriter;
+        }
+
+        public static Packet Remove(long uid)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.ITEM_LOCK);
+            pWriter.WriteEnum(ItemLockMode.Remove);
+            pWriter.WriteLong(uid);
+
+            return pWriter;
+        }
+
+        public static Packet UpdateItems(List<Item> items)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.ITEM_LOCK);
+            pWriter.WriteEnum(ItemLockMode.Update);
+            pWriter.WriteByte((byte) items.Count);
+            foreach (Item item in items)
+            {
+                pWriter.WriteLong(item.Uid);
+                pWriter.WriteItem(item);
+            }
+
+            return pWriter;
+        }
+    }
+}
