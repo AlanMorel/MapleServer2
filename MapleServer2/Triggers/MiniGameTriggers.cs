17a71ba79a97cd07fcc89b3349e4d17dddc3ccaf
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2021-04-01 10:24:16 -0700

    Add skeleton code for handling triggers (#226)

---
+++ MapleServer2/Triggers/MiniGameTriggers.cs
@@ -0,0 +1,99 @@
+﻿using Maple2.Trigger.Enum;
+
+namespace MapleServer2.Triggers
+{
+    public partial class TriggerContext
+    {
+        public void StartMiniGame(int boxId, byte round, MiniGame type, bool isShowResultUI)
+        {
+        }
+
+        public void StartMiniGameRound(int boxId, byte round)
+        {
+        }
+        
+        public void EndMiniGame(int winnerBoxId, MiniGame type, bool isOnlyWinner)
+        {
+        }
+
+        public void EndMiniGameRound(int winnerBoxId, float expRate, bool isOnlyWinner, bool isGainLoserBonus, bool meso, MiniGame type)
+        {
+        }
+        
+        public void MiniGameCameraDirection(int boxId, int cameraId)
+        {
+        }
+
+        public void MiniGameGiveExp(int boxId, float expRate, bool isOutSide)
+        {
+        }
+
+        public void MiniGameGiveReward(int winnerBoxId, string contentType, MiniGame type)
+        {
+        }
+        
+        public void SetMiniGameAreaForHack(int boxId)
+        {
+        }
+        
+        public void UnSetMiniGameAreaForHack()
+        {
+        }
+        
+        public void SetState(byte arg1, string arg2, bool arg3)
+        {
+        }
+
+        public void UseState(byte arg1, bool arg2)
+        {
+        }
+        
+        #region FieldGame
+        public void CreateFieldGame(FieldGame type, bool reset)
+        {
+        }
+        
+        public void FieldGameConstant(string key, string value, string feature, Locale locale)
+        {
+        }
+
+        public void FieldGameMessage(byte custom, string type, byte arg1, string arg2, int arg3)
+        {
+        }
+        #endregion
+        
+        #region GuildVsGame
+        public void GuildVsGameEndGame()
+        {
+        }
+
+        public void GuildVsGameGiveContribution(int teamId, bool isWin, string desc)
+        {
+        }
+
+        public void GuildVsGameGiveReward(GuildReward type, int teamId, bool isWin, string desc)
+        {
+        }
+
+        public void GuildVsGameLogResult(string desc)
+        {
+        }
+
+        public void GuildVsGameLogWonByDefault(int teamId, string desc)
+        {
+        }
+
+        public void GuildVsGameResult(string desc)
+        {
+        }
+
+        public void GuildVsGameScoreByUser(int triggerBoxId, bool score, string desc)
+        {
+        }
+        
+        public void SetUserValueFromGuildVsGameScore(int teamId, string key)
+        {
+        }
+        #endregion
+    }
+}
---
+++ MapleServer2/Triggers/UITriggers.cs
@@ -0,0 +1,108 @@
+﻿using Maple2.Trigger;
+using Maple2.Trigger.Enum;
+
+namespace MapleServer2.Triggers
+{
+    public partial class TriggerContext
+    {
+        public void CreateWidget(WidgetType type)
+        {
+        }
+        
+        public void WidgetAction(WidgetType type, string name, string args, int widgetArgNum)
+        {
+        }
+        
+        public void GuideEvent(int eventId)
+        {
+        }
+
+        public void HideGuideSummary(int entityId, int textId)
+        {
+        }
+        
+        public void Notice(bool arg1, string arg2, bool arg3)
+        {
+        }
+
+        public void PlaySystemSoundByUserTag(int userTagId, string soundKey)
+        {
+        }
+
+        public void PlaySystemSoundInBox(int[] arg1, string arg2)
+        {
+        }
+        
+        public void ScoreBoardCreate(string type, int maxScore)
+        {
+        }
+
+        public void ScoreBoardRemove()
+        {
+        }
+
+        public void ScoreBoardSetScore(bool score)
+        {
+        }
+        
+        public void SetEventUI(byte arg1, string script, int arg3, string arg4)
+        {
+        }
+        
+        public void SetVisibleUI(string uiName, bool visible)
+        {
+        }
+
+        public void ShowCountUI(string text, byte stage, byte count, byte soundType)
+        {
+        }
+        
+        public void ShowRoundUI(byte round, int duration)
+        {
+        }
+        
+        public void ShowGuideSummary(int entityId, int textId, int duration)
+        {
+        }
+        
+        public void SideNpcTalk(int npcId, string illust, int duration, string script, string voice, SideNpcTalkType type, string usm)
+        {
+        }
+        
+        public void ShowCaption(CaptionType type, string title, string script, Align align, float offsetRateX, float offsetRateY, int duration, float scale)
+        {
+        }
+
+        public void ShowEventResult(EventResultType type, string text, int duration, int userTagId, int triggerBoxId, bool isOutSide)
+        {
+        }
+        
+        public void SetCinematicUI(byte type, string script, bool arg3)
+        {
+        }
+        
+        public void SetCinematicIntro(string text)
+        {
+        }
+        
+        public void CloseCinematic()
+        {
+        }
+
+        public void RemoveCinematicTalk()
+        {
+        }
+        
+        public void PlaySceneMovie(string fileName, int movieId, string skipType)
+        {
+        }
+        
+        public void SetSceneSkip(TriggerState state, string arg2)
+        {
+        }
+
+        public void SetSkip(TriggerState state)
+        {
+        }
+    }
+}
