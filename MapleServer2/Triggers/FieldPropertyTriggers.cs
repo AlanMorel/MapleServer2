ad895f3cb2de702892aa1bf08677af8f79184ca7
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2021-03-31 23:32:56 -0700

    Add skeleton code for handling triggers

---
+++ MapleServer2/Triggers/FieldPropertyTriggers.cs
@@ -0,0 +1,56 @@
+﻿using System.Numerics;
+using Maple2.Trigger.Enum;
+
+namespace MapleServer2.Triggers
+{
+    public partial class TriggerContext
+    {
+        public void ChangeBackground(string dds)
+        {
+        }
+        
+        public void EnableLocalCamera(bool isEnable)
+        {
+        }
+
+        public void LockMyPc(bool isLock)
+        {
+        }
+        
+        public void SetAmbientLight(Vector3 arg1, Vector3 arg2, Vector3 arg3)
+        {
+        }
+        
+        public void SetDirectionalLight(Vector3 arg1, Vector3 arg2)
+        {
+        }
+
+        public void SetGravity(float gravity)
+        {
+        }
+        
+        public void SetPhotoStudio(bool isEnable)
+        {
+        }
+        
+        public void SetPvpZone(byte arg1, byte arg2, int arg3, int arg4, byte arg5, byte[] arg6)
+        {
+        }
+        
+        public void SightRange(bool enable, byte range, int rangeZ, byte border)
+        {
+        }
+        
+        public void UserTagSymbol(string symbol1, string symbol2)
+        {
+        }
+        
+        public void VisibleMyPc(bool isVisible)
+        {
+        }
+
+        public void Weather(WeatherType type)
+        {
+        }
+    }
+}
---
+++ MapleServer2/Triggers/PlayerTriggers.cs
@@ -0,0 +1,128 @@
+﻿using System.Numerics;
+using Maple2.Trigger.Enum;
+
+namespace MapleServer2.Triggers
+{
+    public partial class TriggerContext
+    {
+        public void AddEffectNif(int spawnPointId, string nifPath, bool isOutline, float scale, int rotateZ)
+        {
+        }
+        
+        public void RemoveEffectNif(int spawnPointId)
+        {
+        }
+
+        public void EnableSpawnPointPc(int spawnPointId, bool isEnable)
+        {
+        }
+
+        public void FaceEmotion(int spawnPointId, string emotionName)
+        {
+        }
+        
+        public void GiveExp(byte arg1, byte arg2)
+        {
+        }
+
+        public void GiveGuildExp(bool boxId, byte type)
+        {
+        }
+
+        public void GiveRewardContent(int rewardId)
+        {
+        }
+
+        public void KickMusicAudience(int targetBoxId, int targetPortalId)
+        {
+        }
+
+        public void MoveRandomUser(int arg1, byte arg2, int arg3, byte arg4)
+        {
+        }
+
+        public void MoveToPortal(int userTagId, int portalId, int boxId)
+        {
+        }
+
+        public void MoveUser(int arg1, int arg2, int arg3)
+        {
+        }
+
+        public void MoveUserPath(string arg1)
+        {
+        }
+
+        public void MoveUserToBox(int boxId, bool portalId)
+        {
+        }
+
+        public void MoveUserToPos(Vector3 pos, Vector3 rot)
+        {
+        }
+
+        public void PatrolConditionUser(string patrolName, byte patrolIndex, int additionalEffectId)
+        {
+        }
+
+        public void RandomAdditionalEffect(string target, int triggerBoxId, int spawnPointId, byte targetCount, int tick, int waitTick, string targetEffect, int additionalEffectId)
+        {
+        }
+        
+        public void SetPcEmotionLoop(string arg1, float arg2, bool arg3)
+        {
+        }
+
+        public void SetPcEmotionSequence(string arg1)
+        {
+        }
+
+        public void SetPcRotation(Vector3 rotation)
+        {
+        }
+        
+        public void SetAchievement(int arg1, string arg2, string arg3)
+        {
+        }
+
+        public void SetConversation(byte arg1, int arg2, string script, int arg4, byte arg5, Align align)
+        {
+        }
+
+        public void SetOnetimeEffect(int id, bool enable, string path)
+        {
+        }
+        
+        public void SetTimeScale(bool enable, float startScale, float endScale, float duration, byte interpolator)
+        {
+        }
+        
+        public void AddBuff(int[] arg1, int arg2, byte arg3, bool arg4, bool arg5, string feature)
+        {
+        }
+        
+        public void RemoveBuff(int arg1, int arg2, bool arg3)
+        {
+        }
+
+        public void AddUserValue(string key, int value)
+        {
+        }
+        
+        public void SetUserValue(int triggerId, string key, int value)
+        {
+        }
+
+        public void SetUserValueFromDungeonRewardCount(string key, int dungeonRewardId)
+        {
+        }
+
+        public void SetUserValueFromUserCount(int triggerBoxId, string key, int userTagId)
+        {
+        }
+
+        public void UserValueToNumberMesh(string key, int startMeshId, byte digitCount)
+        {
+        }
+    }
+}
