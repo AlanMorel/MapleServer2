d5f12abf1d2404d1de64507a2e93dd0f28357eaa
Zin <adrian.cs101@gmail.com>
2021-04-10 20:11:52 -0700

    Misc Event Packets

---
+++ MapleServer2/Packets/ResultsPacket.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using MaplePacketLib2.Tools;
+using MapleServer2.Constants;
+using MapleServer2.Types;
+
+namespace MapleServer2.Packets
+{
+    public static class ResultsPacket
+    {
+        private enum ResultsPacketMode : byte
+        {
+            Timed = 0x0,
+            Rounds = 0x1,
+            Untimed = 0x3,
+        }
+
+        public static Packet Timed(bool success, List<Item> itemRewards, bool bonus, List<Item> itemRewardsBonus = null)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.RESULTS);
+            pWriter.WriteEnum(ResultsPacketMode.Timed);
+            pWriter.WriteBool(success);
+            pWriter.WriteInt(); // dungeonID
+            pWriter.WriteByte();
+            pWriter.WriteInt();
+            pWriter.WriteInt();
+            pWriter.WriteInt();
+            pWriter.WriteInt();
+            pWriter.WriteByte();
+            pWriter.WriteByte();
+            pWriter.WriteByte();
+            pWriter.WriteByte();
+            pWriter.WriteInt(3); // stats loop
+            pWriter.WriteByte(1); // 1 = mesos
+            pWriter.WriteInt(); // mesos gained
+            pWriter.WriteByte(2); // 2 = exp
+            pWriter.WriteInt(); // exp gained
+            pWriter.WriteByte(3); // 3 = time
+            pWriter.WriteInt(); // clear time
+            pWriter.WriteInt(itemRewards.Count); // quantity of items awarded. Also determines the loop amount
+
+            foreach (Item item in itemRewards)
+            {
+                pWriter.WriteInt(item.Id);
+                pWriter.WriteInt(item.Amount);
+                pWriter.WriteInt(item.Rarity);
+            }
+
+            pWriter.WriteByte(0x0);
+            pWriter.WriteByte(0x0);
+            pWriter.WriteByte(0x0);
+
+            if (bonus == true)
+            {
+                pWriter.WriteInt(); // quantity of items awarded for bonus. Also determines the loop amount
+
+                foreach (Item item in itemRewardsBonus)
+                {
+                    pWriter.WriteInt(item.Id);
+                    pWriter.WriteInt(item.Amount);
+                    pWriter.WriteInt(item.Rarity);
+                }
+            }
+
+            pWriter.WriteInt(0x0);
+            return pWriter;
+        }
+
+        public static Packet Rounds(int roundsCleared, int totalRounds, int expGain, List<Item> itemRewards)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.RESULTS);
+            pWriter.WriteEnum(ResultsPacketMode.Rounds);
+            pWriter.WriteInt(roundsCleared);
+            pWriter.WriteInt(totalRounds);
+            pWriter.WriteInt(expGain);
+
+            foreach (Item item in itemRewards)
+            {
+                pWriter.WriteInt(item.Id);
+                pWriter.WriteInt(item.Amount);
+                pWriter.WriteInt(item.Rarity);
+            }
+
+            pWriter.WriteInt();
+            return pWriter;
+        }
+
+        public static Packet Untimed(bool success)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.RESULTS);
+            pWriter.WriteEnum(ResultsPacketMode.Untimed);
+            pWriter.WriteBool(success);
+            pWriter.WriteInt(); // dungeonID
+            pWriter.WriteInt();
+            pWriter.WriteInt();
+            return pWriter;
+        }
+    }
+}
---
+++ MapleServer2/Packets/TriggerPacket.cs
@@ -0,0 +1,51 @@
+﻿using MaplePacketLib2.Tools;
+using MapleServer2.Constants;
+
+namespace MapleServer2.Packets
+{
+    public static class TriggerPacket
+    {
+        private enum TriggerPacketMode : byte
+        {
+            Reset = 0x2,
+            Trigger = 0x3,
+            Banner = 0x8,
+            Timer = 0xE,
+        }
+
+        public static Packet Trigger(int arg1, byte arg2)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.TRIGGER);
+            pWriter.WriteEnum(TriggerPacketMode.Trigger);
+            pWriter.WriteInt(arg1);
+            pWriter.WriteByte(arg2);
+            pWriter.WriteByte();
+            pWriter.WriteInt();
+            return pWriter;
+        }
+
+        public static Packet Banner(byte state, int stringGuideId)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.TRIGGER);
+            pWriter.WriteEnum(TriggerPacketMode.Banner);
+            pWriter.WriteByte(state); // 02 = on, 03 = off
+            pWriter.WriteInt(stringGuideId);
+            pWriter.WriteInt(stringGuideId);
+            pWriter.WriteInt();
+            return pWriter;
+        }
+
+        public static Packet Timer(int time, bool startCountdown)
+        {
+            PacketWriter pWriter = PacketWriter.Of(SendOp.TRIGGER);
+            pWriter.WriteEnum(TriggerPacketMode.Timer);
+            pWriter.WriteByte();
+            pWriter.WriteInt();
+            pWriter.WriteInt(time); // in ms
+            pWriter.WriteBool(startCountdown); // maybe?
+            pWriter.WriteInt();
+            pWriter.WriteShort();
+            return pWriter;
+        }
+    }
+}
