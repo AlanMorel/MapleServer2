2d378d64ba66108ac81c2a7dde36037046907abd
Zin <adrian.cs101@gmail.com>
2021-04-20 17:48:51 -0700

    Safe Return

--- MapleServer2/PacketHandlers/Game/MoveFieldHandler.cs
+++ MapleServer2/PacketHandlers/Game/MoveFieldHandler.cs
@@ -37,11 +37,22 @@ namespace MapleServer2.PacketHandlers.Game
                     return;
                 }
 
+                if (!MapEntityStorage.HasSafePortal(srcMapId)) // map is instance only
+                {
+                    session.Player.MapId = session.Player.ReturnMapId;
+                    session.Player.Rotation = session.FieldPlayer.Rotation;
+                    session.Player.Coord = session.Player.ReturnCoord;
+                    session.Player.ReturnCoord.Z += Block.BLOCK_SIZE;
+                    session.Send(FieldPacket.RequestEnter(session.FieldPlayer));
+                    return;
+                }
+
                 MapPortal dstPortal = MapEntityStorage.GetPortals(srcPortal.Target)
                     .FirstOrDefault(portal => portal.Target == srcMapId);
                 if (dstPortal == default)
                 {
-                    Logger.Warning($"Unable to find return portal to map:{srcMapId} in map:{srcPortal.Target}");
+                    session.Player.ReturnCoord = session.FieldPlayer.Coord;
+                    session.Player.ReturnMapId = session.Player.MapId;
                 }
 
                 dstPortal = MapEntityStorage.GetPortals(srcPortal.Target)
@@ -59,19 +70,19 @@ namespace MapleServer2.PacketHandlers.Game
 
                 if (dstPortal.Name == "Portal_cube") // spawn on the next block if portal is a cube
                 {
-                    if (dstPortal.Rotation.Z == 0) // Facing SE
+                    if (dstPortal.Rotation.Z == Direction.SOUTH_EAST)
                     {
                         session.Player.Coord.Y -= Block.BLOCK_SIZE;
                     }
-                    else if (dstPortal.Rotation.Z == 90) // Facing NE
+                    else if (dstPortal.Rotation.Z == Direction.NORTH_EAST)
                     {
                         session.Player.Coord.X += Block.BLOCK_SIZE;
                     }
-                    else if (dstPortal.Rotation.Z == 180) // Facing NW
+                    else if (dstPortal.Rotation.Z == Direction.NORTH_WEST)
                     {
                         session.Player.Coord.Y += Block.BLOCK_SIZE;
                     }
-                    else if (dstPortal.Rotation.Z == 270) // Facing SW
+                    else if (dstPortal.Rotation.Z == Direction.SOUTH_WEST)
                     {
                         session.Player.Coord.X -= Block.BLOCK_SIZE;
                     }
@@ -80,6 +91,28 @@ namespace MapleServer2.PacketHandlers.Game
                 session.Player.SafeBlock = session.Player.Coord;
                 session.Send(FieldPacket.RequestEnter(session.FieldPlayer));
             }
+        }
+
+        public static void HandleInstanceMove(GameSession session, int mapId)
+        {
+            // TODO: Revise to include instancing
+
+            if (MapEntityStorage.HasSafePortal(session.Player.MapId))
+            {
+                session.Player.ReturnCoord = session.FieldPlayer.Coord;
+                session.Player.ReturnMapId = session.Player.MapId;
+            }
+
+            MapPortal dstPortal = MapEntityStorage.GetPortals(mapId).First(x => x.Id == 1);
+            if (dstPortal == null)
+            {
+                return;
+            }
+
+            session.Player.MapId = mapId;
+            session.Player.Rotation = dstPortal.Rotation.ToFloat();
+            session.Player.Coord = dstPortal.Coord.ToFloat();
+            session.Send(FieldPacket.RequestEnter(session.FieldPlayer));
         }
     }
 }
