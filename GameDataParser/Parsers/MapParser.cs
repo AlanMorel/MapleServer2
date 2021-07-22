c47863d9d1a13fa2e3e198958cefa1e1bc840fde
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2021-07-22 01:16:43 -0700

    Use XBlockParser to parse map cubes

--- GameDataParser/Parsers/MapParser.cs
+++ GameDataParser/Parsers/MapParser.cs
@@ -3,7 +3,11 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Xml;
 using GameDataParser.Files;
+using Maple2.File.Flat;
+using Maple2.File.Flat.maplestory2library;
 using Maple2.File.IO.Crypto.Common;
+using Maple2.File.Parser.Flat;
+using Maple2.File.Parser.MapXBlock;
 using Maple2Storage.Types;
 using Maple2Storage.Types.Metadata;
 
@@ -11,41 +15,16 @@ namespace GameDataParser.Parsers
 {
     public class MapParser : Exporter<List<MapMetadata>>
     {
+        private List<MapMetadata> mapsList;
+        private Dictionary<int, string> mapNames;
         public MapParser(MetadataResources resources) : base(resources, "map") { }
 
         protected override List<MapMetadata> Parse()
         {
-            // Iterate over preset Cubes to later reference while iterating over exported maps
-            Dictionary<string, string> mapCubes = new Dictionary<string, string>();
-            foreach (PackFileEntry entry in Resources.ExportedReader.Files)
-            {
-                if (!entry.Name.StartsWith("flat/presets/presets cube/"))
-                {
-                    continue;
-                }
-
-                // Check if file is valid
-                string objStr = entry.Name.ToLower();
-                if (string.IsNullOrEmpty(objStr))
-                {
-                    continue;
-                }
-                if (mapCubes.ContainsKey(objStr))
-                {
-                    continue;
-                }
-
-                // Parse XML
-                XmlDocument document = Resources.ExportedReader.GetXmlDocument(entry);
-                XmlElement root = document.DocumentElement;
-                string cubeName = root.Attributes["name"].Value.ToLower();
-                XmlNode propertyAttribute = root.SelectSingleNode("property[@name='MapAttribute']");
-                string mapAttribute = propertyAttribute.FirstChild.Attributes["value"].Value;
-                mapCubes.Add(cubeName, mapAttribute);
-            }
-
+            mapsList = new List<MapMetadata>();
+            
             // Parse map names
-            Dictionary<int, string> mapNames = new Dictionary<int, string>();
+            mapNames = new Dictionary<int, string>();
             foreach (PackFileEntry entry in Resources.XmlReader.Files.Where(x => x.Name.StartsWith("string/en/mapname.xml")))
             {
                 XmlDocument document = Resources.XmlReader.GetXmlDocument(entry);
@@ -58,81 +37,61 @@ namespace GameDataParser.Parsers
             }
 
             // Parse every block for each map
-            List<MapMetadata> mapsList = new List<MapMetadata>();
-            foreach (PackFileEntry entry in Resources.ExportedReader.Files.Where(x => x.Name.StartsWith("xblock/")))
-            {
-                if (entry.Name.Contains("_cn.xblock") || entry.Name.Contains("_jp.xblock") || entry.Name.Contains("_kr.xblock"))
-                {
-                    continue;
-                }
-
-                string mapIdStr = Regex.Match(entry.Name, @"\d{8}").Value;
-                if (string.IsNullOrEmpty(mapIdStr))
-                {
-                    continue;
-                }
-
-                MapMetadata metadata = new MapMetadata();
-                metadata.Id = int.Parse(mapIdStr);
-
-                string xblockName = entry.Name[7..];
-                metadata.XBlockName = xblockName.Remove(xblockName.Length - 7, 7);
-
-                XmlDocument document = Resources.ExportedReader.GetXmlDocument(entry);
-                XmlNodeList mapEntities = document.SelectNodes("/game/entitySet/entity");
-
-                List<MapBlock> blocks = new List<MapBlock>();
-                foreach (XmlNode node in mapEntities)
-                {
-                    MapBlock mapBlock = new MapBlock();
-                    string modelName = node.Attributes["modelName"].Value.ToLower();
-                    if (!mapCubes.ContainsKey(modelName))
-                    {
-                        continue;
-                    }
-                    XmlNode fallReturn = node.SelectSingleNode("property[@name='IsFallReturn']");
-                    bool isFallReturn = (fallReturn?.FirstChild.Attributes["value"].Value) != "False";
-                    if (!isFallReturn)
-                    {
-                        continue;
-                    }
-                    string id = node.Attributes["id"].Value.ToLower();
-                    XmlNode blockCoord = node.SelectSingleNode("property[@name='Position']");
-                    CoordS coordS = CoordS.Parse(blockCoord?.FirstChild.Attributes["value"].Value ?? "0, 0, 0");
-                    mapBlock.Coord = coordS;
-
-                    XmlNode blockType = node.SelectSingleNode("property[@name='CubeType']");
-                    if (blockType != null)
-                    {
-                        mapBlock.Type = blockType?.FirstChild.Attributes["value"].Value;
-                    }
-
-                    XmlNode saleable = node.SelectSingleNode("property[@name='CubeSalableGroup']");
-                    if (saleable != null)
-                    {
-                        mapBlock.SaleableGroup = int.Parse(saleable?.FirstChild.Attributes["value"].Value);
-                    }
-
-                    if (mapCubes.ContainsKey(modelName))
-                    {
-                        mapBlock.Attribute = mapCubes[modelName];
-                    }
-
-                    metadata.Blocks.Add(mapBlock);
-                }
-
-                if (metadata.Blocks.Count == 0)
-                {
-                    continue;
-                }
-
-                if (mapNames.ContainsKey(metadata.Id))
-                {
-                    metadata.Name = mapNames[metadata.Id];
-                }
-                mapsList.Add(metadata);
-            }
+            FlatTypeIndex index = new FlatTypeIndex(Resources.ExportedReader);
+            XBlockParser parser = new XBlockParser(Resources.ExportedReader, index);
+            parser.Include(typeof(IMS2CubeProp)); // We only care about cubes here
+            
+            parser.Parse(AddMetadata);
+            
             return mapsList;
+        }
+
+        private void AddMetadata(string xblock, IEnumerable<IMapEntity> entities)
+        {
+            if (xblock.EndsWith("_cn") || xblock.EndsWith("_jp") || xblock.EndsWith("_kr"))
+            {
+                return;
+            }
+                
+            string mapIdStr = Regex.Match(xblock, @"\d{8}").Value;
+            if (string.IsNullOrEmpty(mapIdStr))
+            {
+                return;
+            }
+
+            MapMetadata metadata = new MapMetadata();
+            metadata.Id = int.Parse(mapIdStr);
+            metadata.XBlockName = xblock;
+
+            foreach (IMapEntity entity in entities)
+            {
+                if (entity is not IMS2CubeProp cube)
+                {
+                    continue;
+                }
+
+                MapBlock mapBlock = new MapBlock
+                {
+                    Coord = CoordS.From((short) cube.Position.X, (short) cube.Position.Y,
+                        (short) cube.Position.Z),
+                    Type = cube.CubeType,
+                    SaleableGroup = cube.CubeSalableGroup,
+                    Attribute = cube.MapAttribute
+                };
+                    
+                metadata.Blocks.Add(mapBlock);
+            }
+                
+            if (metadata.Blocks.Count == 0)
+            {
+                return;
+            }
+                
+            if (mapNames.ContainsKey(metadata.Id))
+            {
+                metadata.Name = mapNames[metadata.Id];
+            }
+            mapsList.Add(metadata);
         }
     }
 }
