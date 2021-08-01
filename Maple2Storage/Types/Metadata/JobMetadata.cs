7fd8abf1a096463f5c3b1c4042d0a7d1626e5029
Zin <62830952+Zintixx@users.noreply.github.com>
2021-08-01 10:00:27 -0700

    Job Parsing and Tutorial Items and Maps (#325)
    * new map entity parser initial commit
    
    * Revert "new map entity parser initial commit"
    
    This reverts commit 7748f4714619ebaf6d03f9ce2560619a23f6e51b.
    
    * Revert "Revert "new map entity parser initial commit""
    
    This reverts commit 042669df3641ea8ab6375abcf796d7f6d761103b.
    
    * meh
    
    * handle unhandledExceptions and find TriggerObjects
    
    * temp commit
    
    * working
    
    * working 2
    
    * Some trigger functions and triggerbox parsing
    
    * zin hehe
    
    * Merge branch 'map-entity-parser-update'
    
    * Revert "Merge branch 'map-entity-parser-update'"
    
    This reverts commit b99f64e1eefd30a18b00062edc97d56a52e35afc.
    
    * Job Parsing and Tutorial Items and Maps
    
    * PR comments
    
    * Cleanup merge
    
    Co-authored-by: Clockworks <40514153+Clockworkx@users.noreply.github.com>

---
+++ Maple2Storage/Types/Metadata/JobMetadata.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Maple2Storage.Types.Metadata
+{
+    [XmlType]
+    public class JobMetadata
+    {
+        [XmlElement(Order = 1)]
+        public int JobId;
+        [XmlElement(Order = 2)]
+        public int StartMapId;
+        [XmlElement(Order = 3)]
+        public List<int> OpenTaxis = new List<int>();
+        [XmlElement(Order = 4)]
+        public List<int> OpenMaps = new List<int>();
+        [XmlElement(Order = 5)]
+        public List<TutorialItemMetadata> TutorialItems = new List<TutorialItemMetadata>();
+        [XmlElement(Order = 6)]
+        public List<JobSkillMetadata> Skills = new List<JobSkillMetadata>();
+        [XmlElement(Order = 7)]
+        public List<JobLearnedSkillsMetadata> LearnedSkills = new List<JobLearnedSkillsMetadata>();
+
+        public JobMetadata() { }
+    }
+
+    [XmlType]
+    public class TutorialItemMetadata
+    {
+        [XmlElement(Order = 1)]
+        public int ItemId;
+        [XmlElement(Order = 2)]
+        public byte Rarity;
+        [XmlElement(Order = 3)]
+        public byte Amount;
+
+        public TutorialItemMetadata() { }
+
+        public TutorialItemMetadata(int itemId, byte rarity, byte amount)
+        {
+            ItemId = itemId;
+            Rarity = rarity;
+            Amount = amount;
+        }
+    }
+
+    [XmlType]
+    public class JobSkillMetadata
+    {
+        [XmlElement(Order = 1)]
+        public int SkillId;
+        [XmlElement(Order = 2)]
+        public short SubJobCode;
+        [XmlElement(Order = 3)]
+        public byte MaxLevel;
+        [XmlElement(Order = 4)]
+        public List<int> SubSkillIds = new List<int>();
+
+        public JobSkillMetadata() { }
+
+        public JobSkillMetadata(int skillId, short subJobCode, byte maxLevel, List<int> subSkillIds)
+        {
+            SkillId = skillId;
+            SubJobCode = subJobCode;
+            MaxLevel = maxLevel;
+            SubSkillIds = subSkillIds;
+        }
+    }
+
+    [XmlType]
+    public class JobLearnedSkillsMetadata
+    {
+        [XmlElement(Order = 1)]
+        public int Level;
+        [XmlElement(Order = 2)]
+        public List<int> SkillIds = new List<int>();
+
+        public JobLearnedSkillsMetadata() { }
+
+        public JobLearnedSkillsMetadata(int level, List<int> skillIds)
+        {
+            Level = level;
+            SkillIds = skillIds;
+        }
+    }
+}
---
+++ MapleServer2/Data/Static/JobMetadataStorage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Maple2Storage.Types.Metadata;
+using MapleServer2.Constants;
+using ProtoBuf;
+
+namespace MapleServer2.Data.Static
+{
+    public static class JobMetadataStorage
+    {
+        private static readonly Dictionary<int, JobMetadata> jobs = new Dictionary<int, JobMetadata>();
+
+        static JobMetadataStorage()
+        {
+            using FileStream stream = File.OpenRead($"{Paths.RESOURCES}/ms2-job-metadata");
+            List<JobMetadata> jobList = Serializer.Deserialize<List<JobMetadata>>(stream);
+            foreach (JobMetadata job in jobList)
+            {
+                jobs[job.JobId] = job;
+            }
+        }
+
+        public static JobMetadata GetJobMetadata(int jobId) => jobs.GetValueOrDefault(jobId);
+
+        public static List<TutorialItemMetadata> GetTutorialItems(int jobId) => jobs.GetValueOrDefault(jobId).TutorialItems;
+
+        public static List<JobLearnedSkillsMetadata> GetLearnedSkills(int jobId) => jobs.GetValueOrDefault(jobId).LearnedSkills;
+
+        public static List<JobSkillMetadata> GetJobskills(int jobId) => jobs.GetValueOrDefault(jobId).Skills;
+
+        public static int GetStartMapId(int jobId) => jobs.GetValueOrDefault(jobId).StartMapId;
+    }
+}
