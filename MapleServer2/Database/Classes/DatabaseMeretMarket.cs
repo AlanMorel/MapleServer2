d707781b2d194a23fb6fd09ebaf311e83c357a22
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2021-08-22 03:12:25 -0300

    Fix MeretMarketItem banner

--- MapleServer2/Database/Classes/DatabaseMeretMarket.cs
+++ MapleServer2/Database/Classes/DatabaseMeretMarket.cs
@@ -6,7 +6,15 @@ namespace MapleServer2.Database.Classes
 {
     public class DatabaseMeretMarket
     {
-        public static List<MeretMarketItem> FindAllByCategoryId(MeretMarketCategory category) => DatabaseManager.QueryFactory.Query("meretmarketitems").Where("Category", (int) category).Get<MeretMarketItem>().ToList();
+        public static List<MeretMarketItem> FindAllByCategoryId(MeretMarketCategory category)
+        {
+            List<MeretMarketItem> items = DatabaseManager.QueryFactory.Query("meretmarketitems").Where("Category", (int) category).Get<MeretMarketItem>().ToList();
+            foreach (MeretMarketItem item in items.Where(x => x.BannerId != 0))
+            {
+                item.Banner = DatabaseManager.QueryFactory.Query("banners").Where("Id", item.BannerId).Get<Banner>().FirstOrDefault();
+            }
+            return items;
+        }
 
         public static MeretMarketItem FindById(int id) => DatabaseManager.QueryFactory.Query("meretmarketitems").Where("MarketId", id).Get<MeretMarketItem>().FirstOrDefault();
 
