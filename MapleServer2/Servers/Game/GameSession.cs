7d3f22665bc903ff145c8f0e82ff897189ada73f
Zin <62830952+Zintixx@users.noreply.github.com>
2021-09-04 17:38:48 -0700

    Update GameSession.cs

--- MapleServer2/Servers/Game/GameSession.cs
+++ MapleServer2/Servers/Game/GameSession.cs
@@ -66,7 +66,6 @@ namespace MapleServer2.Servers.Game
 
                 if (FieldManagerFactory.Release(FieldManager.MapId, FieldManager.InstanceId, player))
                 {
-                    SendNotice("Instanced destroyed");
                     //If instance is destroyed, reset dungeonSession
                     DungeonSession dungeonSession = GameServer.DungeonManager.GetDungeonSessionByInstanceId(FieldManager.InstanceId);
                     //check if the destroyed map was a dungeon map
