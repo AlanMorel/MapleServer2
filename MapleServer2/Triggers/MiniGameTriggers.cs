d0a74f5d3e0bc7afa9a73ad38adc1571a6ff45fc
Sparkmod <rafadagher@gmail.com>
2021-09-07 18:14:17 -0400

    Cleanup: Moving managers to its own folder

--- MapleServer2/Triggers/MiniGameTriggers.cs
+++ MapleServer2/Triggers/MiniGameTriggers.cs
@@ -2,8 +2,8 @@
 using Maple2Storage.Tools;
 using Maple2Storage.Types.Metadata;
 using MapleServer2.Data.Static;
+using MapleServer2.Managers;
 using MapleServer2.Packets;
-using MapleServer2.Servers.Game;
 using MapleServer2.Tools;
 using MapleServer2.Types;
 
