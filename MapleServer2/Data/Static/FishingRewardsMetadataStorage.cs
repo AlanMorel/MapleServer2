b6c1f9230ccc4bb5c550df3b40b158481f4518a5
Zin <62830952+Zintixx@users.noreply.github.com>
2021-09-23 00:13:07 -0700

    Fix fishing

---
+++ MapleServer2/Data/Static/FishingRewardsMetadataStorage.cs
@@ -0,0 +1,47 @@
+﻿using Maple2Storage.Types;
+using Newtonsoft.Json;
+
+namespace MapleServer2.Data.Static
+{
+    public static class FishingRewardsMetadataStorage
+    {
+        private static readonly Dictionary<int, FishingRewardItem> fishItems = new Dictionary<int, FishingRewardItem>();
+
+        public static void Init()
+        {
+            string json = File.ReadAllText($"{Paths.JSON}/FishingRewards.json");
+            List<FishingRewardItem> items = JsonConvert.DeserializeObject<List<FishingRewardItem>>(json);
+            foreach (FishingRewardItem item in items)
+            {
+                fishItems[item.Id] = item;
+            }
+        }
+
+        public static FishingRewardItem GetFishingRewardItem(FishingItemType type)
+        {
+            Random random = new Random();
+            List<FishingRewardItem> items = fishItems.Values.Where(x => x.Type == type).ToList();
+            int index = random.Next(items.Count);
+            return items[index];
+        }
+    }
+
+    public class FishingRewardItem
+    {
+        public int Id;
+        public FishingItemType Type;
+        public int Amount;
+        public int Rarity;
+
+        public FishingRewardItem() { }
+    }
+
+    public enum FishingItemType
+    {
+        Trash = 0,
+        LightBox = 1,
+        HeavyBox = 2,
+        Skin = 3,
+        GoldFish = 4
+    }
+}
