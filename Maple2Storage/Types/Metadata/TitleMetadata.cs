5f58db4e797789d89e483148448f2b56b1023c4f
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2021-11-07 14:27:00 -0300

    Fix: unlock command
    - Parsing titles
    - Only adding valid emotes/titles to player in unlock command

---
+++ Maple2Storage/Types/Metadata/TitleMetadata.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Serialization;
+
+namespace Maple2Storage.Types.Metadata;
+
+[XmlType]
+public class TitleMetadata
+{
+    [XmlElement(Order = 1)]
+    public int Id;
+    [XmlElement(Order = 2)]
+    public string Name;
+    [XmlElement(Order = 3)]
+    public string Feature;
+
+    public TitleMetadata() { }
+
+    public TitleMetadata(int id, string name, string feature)
+    {
+        Id = id;
+        Name = name;
+        Feature = feature;
+    }
+}
