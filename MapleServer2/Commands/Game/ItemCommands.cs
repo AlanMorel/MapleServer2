902b8ef42b2c53aef5aab5b33aedc4a6a7eeef86
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2021-11-26 19:28:48 -0300

    Update ItemCommands.cs

--- MapleServer2/Commands/Game/ItemCommands.cs
+++ MapleServer2/Commands/Game/ItemCommands.cs
@@ -37,7 +37,7 @@ public class ItemCommand : InGameCommand
         Item item = new(itemId)
         {
             Rarity = rarity >= 0 ? rarity : ItemMetadataStorage.GetRarity(itemId),
-            Amount = amount == 0 ? 1 : amount
+            Amount = amount <= 0 ? 1 : amount
         };
         item.Stats = new(item);
 
