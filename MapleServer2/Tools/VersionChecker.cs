0889b914d80dfbe23d6546fb596d58a1bbb07b29
Edmundo Marvão <shiripiti@hotmail.com>
2021-12-09 22:20:49 +0000

    Update VersionChecker.cs

--- MapleServer2/Tools/VersionChecker.cs
+++ MapleServer2/Tools/VersionChecker.cs
@@ -1,33 +1,41 @@
 ﻿using System.Net;
+using Maple2Storage.Types;
 using NLog;
 
 namespace MapleServer2.Tools;
 
 public static class VersionChecker
 {
-    private static readonly Version Version = new("0.1.0");
     private const string DownloadUrl = "https://github.com/darkvergus/MapleServer2/version.txt";
     private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
     public static void CheckUpdate()
     {
+        string versionString = File.ReadAllText(Paths.SOLUTION_DIR + "/version.txt");
+        if (string.IsNullOrEmpty(versionString))
+        {
+            return;
+        }
+        
         try
         {
             WebRequest webRequest = WebRequest.Create(new Uri(DownloadUrl));
             WebResponse webResponse = webRequest.GetResponse();
             StreamReader streamReader = new(webResponse.GetResponseStream());
-
+            
             Version newVersion = new(streamReader.ReadToEnd());
 
-            if (Version.Major.CompareTo(newVersion.Major) != 0)
+            Version currentVersion = new(versionString);
+            
+            if (currentVersion.Major.CompareTo(newVersion.Major) is not 0)
             {
                 Logger.Error("The Server has a new Major Version. It is seriously recommened that you update it!");
             } 
-            else if (Version.Minor.CompareTo(newVersion.Minor) != 0)
+            else if (currentVersion.Minor.CompareTo(newVersion.Minor) is not 0)
             {
                 Logger.Warn("The Server has a new Minor Version. It is recommened that you update it!");
             } 
-            else if (Version.Build.CompareTo(newVersion.Build) != 0)
+            else if (currentVersion.Build.CompareTo(newVersion.Build) is not 0)
             {
                 Logger.Info("The Server has a new Build Version.");
             }
@@ -35,6 +43,7 @@ public static class VersionChecker
             {
                 Logger.Info("The Server is Up-to-date.");
             }
+            
         }
         catch (WebException)
         {
