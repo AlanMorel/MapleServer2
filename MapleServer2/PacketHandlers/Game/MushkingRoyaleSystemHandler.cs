7b34b5fd8c08e0d4090a54a35e7ff05ef4c052ce
Zin <adrian.cs101@gmail.com>
2021-12-29 22:56:48 -0800

    Update MushkingRoyaleSystemHandler.cs

--- MapleServer2/PacketHandlers/Game/MushkingRoyaleSystemHandler.cs
+++ MapleServer2/PacketHandlers/Game/MushkingRoyaleSystemHandler.cs
@@ -4,7 +4,6 @@ using MaplePacketLib2.Tools;
 using MapleServer2.Constants;
 using MapleServer2.Data.Static;
 using MapleServer2.Database;
-using MapleServer2.PacketHandlers.Game.Helpers;
 using MapleServer2.Packets;
 using MapleServer2.Servers.Game;
 using MapleServer2.Types;
