41289aa2727c577396029766519090263f58ae2c
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2022-01-27 21:41:17 -0300

    Update Stats.cs

--- MapleServer2/Types/Stats.cs
+++ MapleServer2/Types/Stats.cs
@@ -671,11 +671,5 @@ public class Stats
         Data[StatId.Int].IncreaseBase(intIncrease);
         Data[StatId.Luk].IncreaseBase(lukIncrease);
         Data[StatId.Hp].IncreaseBase(hpIncrease);
-
-        // Don't know until which level runeblade gains 5 bonus int, if there is one
-        if (player.Job is Job.Runeblade)
-        {
-            Data[StatId.Int].IncreaseBonus(5);
-        }
     }
 }
