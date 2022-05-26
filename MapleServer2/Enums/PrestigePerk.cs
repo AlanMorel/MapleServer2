a76f0b1790aeed109622b412d0d193bca432bfc2
zyliauwu <106284259+zyliauwu@users.noreply.github.com>
2022-05-26 11:05:52 -0700

    Fix #433: Check if player has safe riding before unmounting (#644)
    * Fixes #433: Check if player has safe riding before unmounting when taking fall damage
    
    * Fixed line breaks

---
+++ MapleServer2/Enums/PrestigePerk.cs
@@ -0,0 +1,21 @@
+﻿namespace MapleServer2.Enums;
+
+//Prestige perk unlock levels
+public enum PrestigePerk : int
+{
+    ReviveDiscount = 5,
+    SwiftSwimming = 10,
+    SpeedyGroundMountSprinting = 15,
+    DungeonScrounger = 20,
+    AdvancedBartering = 25,
+    UnboxingExpertise = 30,
+    BargainAttributeReRolling = 35,
+    MesoLooter = 40,
+    RotorsAirDiscount = 45,
+    FleetFootedMount = 50,
+    SocketEfficiency = 60,
+    AmphibiousRiding = 70,
+    PetRetrainingEfficiency = 80,
+    GemstoneFusionEfficiency = 90,
+    SafeRiding = 100
+}
