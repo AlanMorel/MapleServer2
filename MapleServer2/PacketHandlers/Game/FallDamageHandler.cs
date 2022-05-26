567d3639ce84de7d31832f0356ad723967a73353
zyliauwu <106284259+zyliauwu@users.noreply.github.com>
2022-05-26 00:27:26 -0700

    Fixes #433: Check if player has safe riding before unmounting when taking fall damage

--- MapleServer2/PacketHandlers/Game/FallDamageHandler.cs
+++ MapleServer2/PacketHandlers/Game/FallDamageHandler.cs
@@ -1,6 +1,7 @@
 ﻿using Maple2Storage.Types;
 using MaplePacketLib2.Tools;
 using MapleServer2.Constants;
+using MapleServer2.Enums;
 using MapleServer2.Managers;
 using MapleServer2.Packets;
 using MapleServer2.Servers.Game;
@@ -16,8 +17,7 @@ public class FallDamageHandler : GamePacketHandler<FallDamageHandler>
         float distance = packet.ReadFloat();
         if (distance > Block.BLOCK_SIZE * 6)
         {
-            // TODO: Check if player has Safe ride enabled
-            if (session.Player.Mount != null)
+            if (session.Player.Mount != null && session.Player.Levels.PrestigeLevel < (int) PrestigePerk.SafeRiding)
             {
                 session.FieldManager.BroadcastPacket(MountPacket.StopRide(session.Player.FieldPlayer));
             }
