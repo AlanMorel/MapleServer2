8fa786e8546a85c32e131b218e7182f204f3b933
Zin <adrian.cs101@gmail.com>
2022-06-06 21:45:50 -0700

    Update DatabaseBeautyShopItem.cs

--- MapleServer2/Database/Classes/DatabaseBeautyShopItem.cs
+++ MapleServer2/Database/Classes/DatabaseBeautyShopItem.cs
@@ -9,28 +9,6 @@ public class DatabaseBeautyShopItem : DatabaseTable
 {
     public DatabaseBeautyShopItem() : base("beauty_shop_items") { }
 
-    public ShopItem FindByUid(long uid)
-    {
-        return ReadShopItem(QueryFactory.Query(TableName).Where("uid", uid).Get().FirstOrDefault());
-    }
-
-    public long Insert(BeautyItem beauty, int shopId)
-    {
-        return QueryFactory.Query(TableName).InsertGetId<long>(new
-        {
-            item_id = beauty.ItemId,
-            shop_id = shopId,
-            gender = beauty.Gender,
-            flag = beauty.Flag,
-            required_level = beauty.Flag,
-            required_achievement_id = beauty.RequiredAchievementId,
-            required_achievement_grade = beauty.RequiredAchievementGrade,
-            currency_type = beauty.TokenType,
-            currency_cost = beauty.TokenCost,
-            required_item_id = beauty.RequiredItemId
-        });
-    }
-    
     public List<BeautyShopItem> FindAllByShopId(int shopId)
     {
         List<BeautyShopItem> items = new();
@@ -41,13 +19,6 @@ public class DatabaseBeautyShopItem : DatabaseTable
         }
 
         return items;
-    }
-
-    public BeautyShopItem FindByItemId(int itemId)
-    {
-        dynamic result = QueryFactory.Query(TableName).Where("item_id", itemId).FirstOrDefault();
-
-        return result == null ? null : ReadShopItem(result);
     }
 
     public List<BeautyShopItem> FindAllByShopIdAndGender(int shopId, Gender gender)
