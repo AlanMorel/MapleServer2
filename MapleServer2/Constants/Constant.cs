a47e23f66fb522718cfeff07ddde6f06a7942b06
Nathan G <gleefuldev@gmail.com>
2022-06-22 15:15:28 -0500

    Fix: Mounts now consume stamina when sprinting

--- MapleServer2/Constants/Constant.cs
+++ MapleServer2/Constants/Constant.cs
@@ -3,4 +3,5 @@
 public static class Constant
 {
     public const float UnitPerMs = 0.001f; // Velocity in which all NPCs move per ms. 150 units per second.
+    public const int MountStaminaConsumption = 50; // Amount of stamina consumed each tick when sprinting with a ground mount.
 }
