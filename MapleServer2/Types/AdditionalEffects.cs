06ced024edc2883988f23bbce8cc1b9a9a890ed1
mettaursp <trevorb101@hotmail.com>
2022-12-26 02:05:00 -0800

    formatting

--- MapleServer2/Types/AdditionalEffects.cs
+++ MapleServer2/Types/AdditionalEffects.cs
@@ -1,9 +1,9 @@
 ﻿using Maple2Storage.Enums;
 using Maple2Storage.Types.Metadata;
 using MapleServer2.Data.Static;
+using MapleServer2.Enums;
 using MapleServer2.Managers.Actors;
 using MapleServer2.Packets;
-using MapleServer2.Enums;
 
 namespace MapleServer2.Types;
 
